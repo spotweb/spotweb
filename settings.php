e78b6b9d1c63ea84ae80e8b58a630292d70fff30
rota <spotweb@hush.com>
2011-03-04 18:13:33 +0100

    Ampersand werd niet altijd juist gequote

--- settings.php
+++ settings.php
@@ -105,7 +105,7 @@ $settings['sabnzbd']['categories'] = Array(
 					 
 
 # zoekmachine url (gebruikt bij spots voor 24 november als download knop, en onderaan de spot info)
-$settings['search_url'] = 'http://www.binsearch.info/?adv_age=&q=$SPOTFNAME';
+$settings['search_url'] = 'http://www.binsearch.info/?adv_age=&amp;q=$SPOTFNAME';
 # $settings['search_url'] = 'http://nzbindex.nl/search/?q=$SPOTFNAME';
 
 # de filter die standaard gebruikt wordt op de index pagina (als er geen filters oid opgegeven zijn), 
