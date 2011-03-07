f0757e01e9d02c21bb365a00a2ced19fa498a1e0
rota <spotweb@hush.com>
2011-03-08 00:27:23 +0100

    Vergeten template var in te vullen

--- templates_we1rdo/spotinfo.inc.php
+++ templates_we1rdo/spotinfo.inc.php
@@ -18,7 +18,7 @@
 ?>
 						
                         	<th class="category"><?php echo $spot['formatname'];?></th>
-                            <th class="title">Post Title</th>
+                            <th class="title"><?php echo $spot['title'];?></th>
                             <th class="nzb">
                             	<a class="search" href="<?php echo $spot['searchurl'];?>" title="NZB zoeken">Zoeken</a>
                                 |
