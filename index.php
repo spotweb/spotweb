8df662dd8550a30e84ce67216c6d069909f811a0
rota <spotweb@hush.com>
2011-03-07 08:14:29 +0100

    Globals b-gone!

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 error_reporting(E_ALL & ~8192 & ~E_USER_WARNING);	# 8192 == E_DEPRECATED maar PHP < 5.3 heeft die niet
 
+require_once "settings.php";
 require_once "lib/SpotDb.php";
 require_once "lib/SpotReq.php";
 require_once "lib/SpotParser.php";
@@ -13,80 +14,48 @@ require_once "lib/page/SpotPage_getspot.php";
 require_once "lib/page/SpotPage_catsjson.php";
 require_once "lib/page/SpotPage_erasedls.php";
 
-function openDb($dbSettings) {
-	# fireup the database
-	try {
-		$db = new SpotDb($dbSettings);
-		$db->connect();
-	} 
-	catch(Exception $x) {
-		die('Unable to open database: ' . $x->getMessage());
-	} # catch
+#- main() -#
+try {
+	# database object
+	$db = new SpotDb($settings['db']);
+	$db->connect();
 	
-	return $db;
-} # openDb
-
-function initialize() {
-	require_once "settings.php";
-	$settings = $GLOBALS['settings'];
-
-	# we define some preferences, later these could be
-	# user specific or stored in a cookie or something
-	$prefs = array('perpage' => 100);
-	if (isset($settings['prefs'])) {
-		$prefs = array_merge($prefs, $settings['prefs']);
-	} # if
-		
 	# helper functions for passed variables
 	$req = new SpotReq();
 	$req->initialize();
 
-	# gather the current page
-	$GLOBALS['site']['page'] = $req->getDef('page', 'index');
-	if (array_search($GLOBALS['site']['page'], array('index', 'catsjson', 'getnzb', 'getspot', 'erasedls')) === false) {
-		$GLOBALS['site']['page'] = 'index';
+	$page = $req->getDef('page', 'index');
+	if (array_search($page, array('index', 'catsjson', 'getnzb', 'getspot', 'erasedls')) === false) {
+		$page = 'index';
 	} # if
-	
-	# and put them in an encompassing site object
-	$GLOBALS['site']['req'] = $req;
-	$GLOBALS['site']['settings'] = $settings;
-	$GLOBALS['site']['prefs'] = $prefs;
-	$GLOBALS['site']['pagetitle'] = 'SpotWeb - ';
-	$GLOBALS['site']['db'] = openDb($settings['db']);
-} # initialize()
 
-#- main() -#
-try {
-	initialize();
-	extract($GLOBALS['site'], EXTR_REFS);
-	
-	switch($site['page']) {
+	switch($page) {
 		case 'getspot' : {
-				$page = new SpotPage_getspot($db, $settings, $prefs, $req->getDef('messageid', ''));
+				$page = new SpotPage_getspot($db, $settings, $settings['prefs'], $req->getDef('messageid', ''));
 				$page->render();
 				break;
 		} # getspot
 
 		case 'getnzb' : {
-				$page = new SpotPage_getnzb($db, $settings, $prefs, $req->getDef('messageid', ''));
+				$page = new SpotPage_getnzb($db, $settings, $settings['prefs'], $req->getDef('messageid', ''));
 				$page->render();
 				break;
 		} # getspot
 
 		case 'erasedls' : {
-				$page = new SpotPage_erasedls($db, $settings, $prefs);
+				$page = new SpotPage_erasedls($db, $settings, $settings['prefs']);
 				$page->render();
 				break;
 		} # erasedls
 		
 		case 'catsjson' : {
-				$page = new SpotPage_catsjson($db, $settings, $prefs);
+				$page = new SpotPage_catsjson($db, $settings, $settings['prefs']);
 				$page->render();
 				break;
 		} # getspot
 
 		case 'index' : {
-				$page = new SpotPage_index($db, $settings, $prefs, 
+				$page = new SpotPage_index($db, $settings, $settings['prefs'], 
 							Array('search' => $req->getDef('search', $settings['index_filter']),
 								  'page' => $req->getDef('page', 0),
 								  'sortby' => $req->getDef('sortby', ''),
