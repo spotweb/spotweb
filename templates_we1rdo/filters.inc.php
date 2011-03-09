da43731a6599f4fcacf10cd62270b89ad9d7ea00
we1rdo <klaas248@gmail.com>
2011-03-09 17:56:11 +0800

    subfilters werken weer

--- templates_we1rdo/filters.inc.php
+++ templates_we1rdo/filters.inc.php
@@ -34,27 +34,26 @@
 						<input type='submit' class="filtersubmit" value='Zoek en filter'></input>
 					</form>
 
-					<h4>Filters</h4>
-					<ul class="filterlist">
+<h4>Filters</h4>
+                    
+                    <ul class="filterlist">
 <?php
-	foreach($filters as $filter) {
+    foreach($filters as $filter) {
 ?>
-						<li> 
-							<a class="filter <?php echo $filter[3]; ?>" href="?<?php echo $tplHelper->getFilterParams('tree') . '&amp;search[tree]=' . $filter[2]; ?>"><img src='<?php echo $filter[1]; ?>'><?php echo $filter[0]; ?></a>
+                        <li> <a class="filter <?php echo $filter[3]; ?>" href="?search[tree]=<?php echo $filter[2];?>"><img src='<?php echo $filter[1]; ?>'><?php echo $filter[0]; ?></a>
 <?php
-		if (!empty($filter[4])) {
-			echo "\t\t\t\t\t\t\t<ul class='filterlist subfilterlist'>\r\n";
-			foreach($filter[4] as $subFilter) {
+        if (!empty($filter[4])) {
+            echo "\t\t\t\t\t\t\t<ul class='filterlist subfilterlist'>\r\n";
+            foreach($filter[4] as $subFilter) {
 ?>
-								<li> <a class="filter <?php echo $subFilter[3];?>" href="?<?php echo $tplHelper->getFilterParams('tree') . '&amp;search[tree]=' . $filter[2]; ?>"><img src='<?php echo $subFilter[1]; ?>'><?php echo $subFilter[0]; ?></a></li>
+                                <li> <a class="filter <?php echo $subFilter[3];?>" href="?search[tree]=<?php echo $subFilter[2];?>"><img src='<?php echo $subFilter[1]; ?>'><?php echo $subFilter[0]; ?></a>
 <?php
-			} # foreach 
-			echo "\t\t\t\t\t\t\t</ul>\r\n";
-		} # is_array
-		echo "\t\t\t\t\t\t</li>\r\n";
-	} # foreach
+            } # foreach 
+            echo "\t\t\t\t\t\t\t</ul>\r\n";
+        } # is_array
+    } # foreach
 ?>
-					</ul>
+                    </ul>
 
 					<h4>Maintenance</h4>
 					<ul class="filterlist maintenancebox">
