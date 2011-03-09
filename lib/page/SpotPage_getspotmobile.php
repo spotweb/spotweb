0228a6e91768ea750ab014e82aa438eda71cbd47
bakman <none@none.com>
2011-03-10 00:15:18 +0100

    new file:   lib/page/SpotPage_getnzbmobile.php 	new file:   lib/page/SpotPage_getspotmobile.php

---
+++ lib/page/SpotPage_getspotmobile.php
@@ -0,0 +1,31 @@
+<?php
+require_once "lib/page/SpotPage_Abs.php";
+require_once "lib/SpotCategories.php";
+
+class SpotPage_getspotmobile extends SpotPage_Abs {
+	private $_messageid;
+	
+	function __construct($db, $settings, $prefs, $messageid) {
+		parent::__construct($db, $settings, $prefs);
+		$this->_messageid = $messageid;
+	} # ctor
+
+
+	function render() {
+		$spotnntp = new SpotNntp($this->_settings['nntp_hdr']);
+		$spotnntp->connect();
+
+		# Haal de volledige spotinhoud op
+		$spotsOverview = new SpotsOverview($this->_db);
+		$fullSpot = $spotsOverview->getFullSpot($this->_messageid, $spotnntp);
+		$comments = $spotsOverview->getSpotComments($this->_messageid, $spotnntp);
+		
+		# zet de page title
+		$this->_pageTitle = "spot: " . $fullSpot['title'];
+		
+		#- display stuff -#
+
+		$this->template('spotinfo', array('spot' => $fullSpot, 'comments' => $comments));
+	} # render
+	
+} # class SpotPage_getspot
