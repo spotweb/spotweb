0a4c7cf0be86f8979305ac5e6e2264b89075ad8d
bakman2 <richardbakker@me.com>
2011-03-12 15:27:22 +0800

    get proper path for mobile template

--- templates_mobile/spots.inc.php
+++ templates_mobile/spots.inc.php
@@ -3,7 +3,7 @@ $getUrl = $tplHelper->getFilterParams();
 $site = $_SERVER['SERVER_NAME'];
 $source = $_SERVER['REQUEST_URI'];
 $getpath = explode('/',$source);
-$setpath = $site . "/" . $getpath . "/";
+$setpath = $site . "/" . $getpath[1] . "/";
 ?>
 
 <div data-role="page" id="spots"> 
