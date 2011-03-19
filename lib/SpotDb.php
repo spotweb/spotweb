269829226afacb40c44ede143614b0fd09ac77f2
rota <spotweb@hush.com>
2011-03-19 19:39:14 +0100

    Debug -- niet pullen!

--- lib/SpotDb.php
+++ lib/SpotDb.php
@@ -111,6 +111,12 @@ class SpotDb
 		# vraag eerst het id op
 		$spot = $this->getFullSpot($messageId);
 		
+			/*DEBUG*/ echo "DEBUG: removeExtraSpots(): " . $messageId . PHP_EOL;
+			/*DEBUG*/ echo "DEBUG: spot: " . $spot['messageid'] . PHP_EOL;
+			/*DEBUG*/ echo "DEBUG: spotid: " . $spot['spotdbid'] . PHP_EOL;
+			/*DEBUG*/ echo "DEBUG: highestid: " . $this->_conn->singleQuery("SELECT max(id) as m FROM spots") . PHP_EOL;
+
+		
 		# als deze spot leeg is, is er iets raars aan de hand
 		if (empty($spot)) {
 			throw new Exception("Our highest spot is not in the database!?");
@@ -222,9 +228,9 @@ class SpotDb
 		# het geheugen gebruik, dus liefst niet.
 		if ($getFull) {
 			$extendedFieldList = ',
-							f.usersignature AS usersignature,
-							f.userkey AS userkey,
-							f.xmlsignature AS xmlsignature,
+							f.usersignature AS "user-signature",
+							f.userkey AS "user-key",
+							f.xmlsignature AS "xml-signature",
 							f.fullxml AS fullxml';
 		} else {
 			$extendedFieldList = '';
@@ -270,7 +276,7 @@ class SpotDb
 												f.usersignature AS \"user-signature\",
 												f.userkey AS \"user-key\",
 												f.xmlsignature AS \"xml-signature\",
-												f.fullxml AS xml,
+												f.fullxml AS fullxml,
 												f.filesize AS filesize
 												FROM spots AS s 
 												JOIN spotsfull AS f ON f.messageid = s.messageid 
@@ -282,7 +288,7 @@ class SpotDb
 		
 		# If spot is fully stored in db and is of the new type, we process it to
 		# make it exactly the same as when retrieved using NNTP
-		if (!empty($tmpArray['xml']) && (!empty($tmpArray['user-signature']))) {
+		if (!empty($tmpArray['fullxml']) && (!empty($tmpArray['user-signature']))) {
 			$tmpArray['user-signature'] = base64_decode($tmpArray['user-signature']);
 			$tmpArray['user-key'] = unserialize(base64_decode($tmpArray['user-key']));
 		} # if
@@ -422,8 +428,8 @@ class SpotDb
 					  base64_encode($fullSpot['user-signature']),
 					  base64_encode(serialize($fullSpot['user-key'])),
 					  $fullSpot['xml-signature'],
-					  $fullSpot['xml'],
-					  $fullSpot['size']));
+					  $fullSpot['fullxml'],
+					  $fullSpot['filesize']));
 	} # addFullSpot
 
 	function addToWatchlist($messageId, $comment) {
