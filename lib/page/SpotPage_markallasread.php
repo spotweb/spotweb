9d97458256de52a43d5c298a8dac497fe7f0bfea
Nightspirit <ben@nightspirit.nl>
2011-03-25 13:27:00 +0100

    last_visit krijgt nu de tijd mee van het laatste bericht in de database

--- lib/page/SpotPage_markallasread.php
+++ lib/page/SpotPage_markallasread.php
@@ -1,10 +1,20 @@
 <?php
+require_once "settings.php";
+require_once "lib/SpotDb.php";
 require_once "lib/page/SpotPage_Abs.php";
 
 class SpotPage_markallasread extends SpotPage_Abs {
 	
 	function render() {
-		$_SESSION['last_visit'] = time();
+		try {
+			$db = new SpotDb($this->_settings['db']);
+			$db->connect();
+		} 
+		catch(Exception $x) {
+			die("Unable to connect to database: " . $x->getMessage() . PHP_EOL);
+		} # catch
+
+		$_SESSION['last_visit'] = $db->getMaxMessageTime();
 		echo "<xml><return>ok</return></xml>";
 	} # render()
 
