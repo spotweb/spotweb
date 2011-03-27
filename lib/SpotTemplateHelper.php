dc2b786cac423da39b54612b8d91178b9217882c
rota <spotweb@hush.com>
2011-03-27 18:11:05 +0200

    Methode iets generieker gemaakt - dit staat ons toe bv. ook counts voor de downloads weer te geven

--- lib/SpotTemplateHelper.php
+++ lib/SpotTemplateHelper.php
@@ -32,25 +32,34 @@ class SpotTemplateHelper {
 	} # getSpotCount
 
 	/*
-	* Geef het aantal spots terug maar dan rekening houdende met het filter
-	*/
+ 	 * Geef het aantal spots terug maar dan rekening houdende met het filter
+ 	 */
+	function getFilteredSpotCount($filterStr) {
+		parse_str(html_entity_decode($filterStr), $query_params);
+		
+		$spotsOverview = new SpotsOverview($this->_db, $this->_settings);
+		$sqlFilter = $spotsOverview->filterToQuery($query_params['search']);
+		
+		return $this->getSpotCount($sqlFilter);
+	} # getFilteredSpotCount
+
+	/*
+	 * Geef het aantal spots terug, maar enkel die new zijn
+	 */
 	function getNewCountForFilter($filterStr) {
 		if ($this->_settings['count_newspots'] == false) {
 			return '';
 		} # if
-
+		
 		$filterStr .= "&search[value][]=New:0";
-		parse_str(html_entity_decode($filterStr), $query_params);
-		$spotsOverview = new SpotsOverview($this->_db, $this->_settings);
-		$sqlFilter = $spotsOverview->filterToQuery($query_params['search']);
-		$newCount = $this->getSpotCount($sqlFilter);
+		$newCount = $this->getFilteredSpotCount($filterStr);
 
 		if ($newCount > 0) {
 			return ' (' . $newCount . ')';
 		} else {
 			return '';
-		}
-	} # getFilteredSpotCount
+		} # else
+	} # getNewCountForFilter
 
 	/*
 	 * Geef het aantal spots terug 
