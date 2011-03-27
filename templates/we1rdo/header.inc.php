2ee4bc7cc4a7cf889f920e8032c9f41a329fdb54
Nightspirit <ben@nightspirit.nl>
2011-03-27 10:14:51 +0200

    https://github.com/spotweb/spotweb/issues#issue/330

--- templates/we1rdo/header.inc.php
+++ templates/we1rdo/header.inc.php
@@ -2,22 +2,23 @@
 <html>
 	<head>
 		<title><?php echo $pagetitle?></title>
-		<link rel='stylesheet' type='text/css' href='<?php echo $tplHelper->makeBaseUrl(); ?>js/dynatree/skin-vista/ui.dynatree.css'>
-		<link rel="stylesheet" href='<?php echo $tplHelper->makeBaseUrl(); ?>templates/we1rdo/style.css' type='text/css'>
-		<link rel="stylesheet" type='text/css' href='<?php echo $tplHelper->makeBaseUrl(); ?>js/fancybox/jquery.fancybox-1.3.4.css' media='screen'>
-		<link rel="alternate" type='application/atom+xml' href='<?php echo $tplHelper->getPageUrl('atom', true) ?>'>
-		<link rel='shortcut icon' href='<?php echo $tplHelper->makeBaseUrl(); ?>templates/we1rdo/img/favicon.ico'>
+		<base href='<?php echo $tplHelper->makeBaseUrl(); ?>' />
+		<link rel='stylesheet' type='text/css' href='js/dynatree/skin-vista/ui.dynatree.css'>
+		<link rel='stylesheet' href='templates/we1rdo/style.css' type='text/css'>
+		<link rel='stylesheet' type='text/css' href='js/fancybox/jquery.fancybox-1.3.4.css' media='screen'>
+		<link rel='alternate' type='application/atom+xml' href='<?php echo $tplHelper->getPageUrl('atom', true) ?>'>
+		<link rel='shortcut icon' href='templates/we1rdo/img/favicon.ico'>
 
 		<!-- Jquery, necessary for dynatree -->
-		<script src='<?php echo $tplHelper->makeBaseUrl(); ?>js/jquery/jquery.min.js' type='text/javascript'></script>
-		<script src='<?php echo $tplHelper->makeBaseUrl(); ?>js/jquery/jquery-ui.custom.min.js' type='text/javascript'></script>
-		<script src='<?php echo $tplHelper->makeBaseUrl(); ?>js/jquery/jquery.cookie.js' type='text/javascript'></script>
+		<script src='js/jquery/jquery.min.js' type='text/javascript'></script>
+		<script src='js/jquery/jquery-ui.custom.min.js' type='text/javascript'></script>
+		<script src='js/jquery/jquery.cookie.js' type='text/javascript'></script>
 
 		<!-- dynatree iteslf -->
-		<script src='<?php echo $tplHelper->makeBaseUrl(); ?>js/dynatree/jquery.dynatree.min.js' type='text/javascript'></script>
+		<script src='js/dynatree/jquery.dynatree.min.js' type='text/javascript'></script>
 
 		<!-- fancybox -->
-		<script type="text/javascript" src="<?php echo $tplHelper->makeBaseUrl(); ?>js/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
+		<script type="text/javascript" src="js/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
 
 		<!-- Add code to initialize the tree when the document is loaded: -->
 		<script type='text/javascript'>
@@ -42,7 +43,7 @@
 			// Attach the dynatree widget to an existing <div id="tree"> element
 			// and pass the tree options as an argument to the dynatree() function:
 			$("#tree").dynatree({
-				initAjax: { url: "<?php echo $tplHelper->getPageUrl('catsjson');?>" },
+				initAjax: { url: "?page=catsjson" },
 			    checkbox: true, // Show checkboxes.
 				persist: false, // Persist expand-status to a cookie
 				selectMode: 3, //  1:single, 2:multi, 3:multi-hier
@@ -83,7 +84,7 @@
 						alert('Error removing downloadlist');
 					},
 					beforeSend: function(jqXHR, settings) {
-						var x = $("li.info").html("<img src='<?php echo $tplHelper->makeBaseUrl(); ?>templates/we1rdo/img/loading.gif' />");
+						var x = $("li.info").html("<img src='templates/we1rdo/img/loading.gif' />");
 					}, // # beforeSend
 					complete: function(jqXHR, textStatus) {
 						var x = setTimeout( function() { $("li.info").html("History removed") }, 1000);
@@ -115,7 +116,7 @@
 						} // if 
 					},
 					beforeSend: function(jqXHR, settings) {
-						var x = $("li.info").html("<img src='<?php echo $tplHelper->makeBaseUrl(); ?>templates/we1rdo/img/loading.gif' />");
+						var x = $("li.info").html("<img src='templates/we1rdo/img/loading.gif' />");
 					}, // # beforeSend
 					complete: function(jqXHR, textStatus) {
 						var x = $("li.info").html("Updated spots");
@@ -137,7 +138,7 @@
 						alert('Error marking all as read');
 					},
 					beforeSend: function(jqXHR, settings) {
-						var x = $("li.info").html("<img src='<?php echo $tplHelper->makeBaseUrl(); ?>templates/we1rdo/img/loading.gif' />");
+						var x = $("li.info").html("<img src='templates/we1rdo/img/loading.gif' />");
 					}, // # beforeSend
 					complete: function(jqXHR, textStatus) {
 						var x = setTimeout( function() { $("li.info").html("All marked as read.") }, 1000);
@@ -166,10 +167,10 @@
 						// niet te zien
 					},
 					beforeSend: function(jqXHR, settings) {
-						$(temp).html("<img class='sabnzbd-button loading' src='<?php echo $tplHelper->makeBaseUrl(); ?>templates/we1rdo/img/loading.gif' />");
+						$(temp).html("<img class='sabnzbd-button loading' src='templates/we1rdo/img/loading.gif' />");
 					}, // # beforeSend
 					complete: function(jqXHR, textStatus) {
-						setTimeout( function() { $(temp).html("<img class='sabnzbd-button' src='<?php echo $tplHelper->makeBaseUrl(); ?>templates/we1rdo/img/succes.png' />") }, 1000);
+						setTimeout( function() { $(temp).html("<img class='sabnzbd-button' src='templates/we1rdo/img/succes.png' />") }, 1000);
 					}, // # complete
 					dataType: "text"
 				});
