ef3da841d7c049646b6a1fba6ea79fc44a7ceb4a
Nightspirit <ben@nightspirit.nl>
2011-03-27 17:35:20 +0200

    Vereenvoudiging in Template + Counter die weer altijd werken

--- templates/splendid/filters.inc.php
+++ templates/splendid/filters.inc.php
@@ -42,9 +42,9 @@ if(empty($_GET['ajax']) && $_GET['page'] != "watchlist") { ?>
 					<ul id="quicklinksmenu">
 <?php
     foreach($quicklinks as $quicklink) {
-		if (stripos($quicklink[2], 'search[type]=New') && $settings['count_newspots']) {
-			$strCountFilter = $tplHelper->getPageUrl('index') . $quicklink[2];
-			$newQuickCount = $tplHelper->getFilteredSpotCount($strCountFilter);
+		$strFilter = $tplHelper->getPageUrl('index') . $quicklink[2];
+		if (stripos($quicklink[2], 'New:0') && $settings['count_newspots']) {
+			$newQuickCount = $tplHelper->getFilteredSpotCount($strFilter, '');
 		} else { $newQuickCount = 0; }
 		if($quicklink[0] == 'Watchlist') {
 ?>
@@ -64,9 +64,10 @@ if(empty($_GET['ajax']) && $_GET['page'] != "watchlist") { ?>
                     <ul id="filtermenu">
 <?php
     foreach($filters as $filter) {
+		$strFilter = $tplHelper->getPageUrl('index') . '&amp;search[tree]=' . $filter[2];
 		if ($settings['count_newspots']) {
-			$strCountFilter = $tplHelper->getPageUrl('index') . '&amp;search[tree]=' . $filter[2] . '&amp;search[type]=New';
-			$newCount = $tplHelper->getFilteredSpotCount($strCountFilter);
+			
+			$newCount = $tplHelper->getFilteredSpotCount($strFilter, 'new');
 		} else { $newCount = 0; }
 ?>
                         <li<?php if($filter[2]) { echo " class='". $tplHelper->filter2cat($filter[2]) ."'"; } ?>><div><a class="filter <?php echo $filter[3]; ?>" onclick="$('#spots').load('?search[tree]=<?php echo $filter[2];?>&ajax=1');clearTree();">
@@ -78,8 +79,7 @@ if(empty($_GET['ajax']) && $_GET['page'] != "watchlist") { ?>
             foreach($filter[4] as $subFilter) {
 				$strFilter = $tplHelper->getPageUrl('index') . '&amp;search[tree]=' . $subFilter[2];
 				if ($settings['count_newspots']) {
-					$strCountFilter = $tplHelper->getPageUrl('index') . '&amp;search[tree]=' . $subFilter[2] . '&amp;search[type]=New';
-					$newSubCount = $tplHelper->getFilteredSpotCount($strCountFilter);
+					$newSubCount = $tplHelper->getFilteredSpotCount($strFilter, 'new');
 				} else { $newSubCount = 0; }
 ?>
             			<li><div><a class="subfilter <?php echo $subFilter[3];?>" onclick="$('#spots').load('<?php echo $strFilter;?>&ajax=1');clearTree();">
@@ -91,8 +91,7 @@ if(empty($_GET['ajax']) && $_GET['page'] != "watchlist") { ?>
 					foreach($subFilter[4] as $sub2Filter) {
 						$strFilter = $tplHelper->getPageUrl('index') . '&amp;search[tree]=' . $sub2Filter[2];
 						if ($settings['count_newspots']) {
-							$strCountFilter = $tplHelper->getPageUrl('index') . '&amp;search[tree]=' . $sub2Filter[2] . '&amp;search[type]=New';
-							$newSub2Count = $tplHelper->getFilteredSpotCount($strCountFilter);
+							$newSub2Count = $tplHelper->getFilteredSpotCount($strFilter, 'new');
 						} else { $newSub2Count = 0; }
 						$strFilter = ''; // hoort deze regel hier?
 		?>
