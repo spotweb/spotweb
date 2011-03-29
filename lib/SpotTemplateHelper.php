96863b162d4f401b126c94acc415aeced4d40340
rota <spotweb@hush.com>
2011-03-30 00:02:03 +0200

    Voorkom dat SpotWeb erg traag wordt als er erg veel nieuwe spots zijn

--- lib/SpotTemplateHelper.php
+++ lib/SpotTemplateHelper.php
@@ -54,6 +54,14 @@ class SpotTemplateHelper {
 		$filterStr .= "&search[value][]=New:0";
 		$newCount = $this->getFilteredSpotCount($filterStr);
 
+		# lelijke hack om er voor te zorgen dat als er erg veel nieuwe spots 
+		# zijn, SpotWeb niet ontzettend traag wordt. Op het moment dat er een
+		# persistency laag achter settings komt ewl mee oppassen :P
+		if ($newCount > 5000) {
+			$this->_settings['count_newspots'] = false;
+		} # if
+		
+		# en geef het aantal terug dat we willen hebben
 		if ($newCount > 0) {
 			return ' (' . $newCount . ')';
 		} else {
