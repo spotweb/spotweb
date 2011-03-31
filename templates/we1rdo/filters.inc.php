d78a0c4292a5eeb3eb0b8fa8ca3b7d6aa692ad3a
we1rdo <klaas248@gmail.com>
2011-03-31 22:27:41 +0200

    IE <= 8 bugfixes i.v.m. newspots

--- templates/we1rdo/filters.inc.php
+++ templates/we1rdo/filters.inc.php
@@ -45,7 +45,7 @@
     foreach($quicklinks as $quicklink) {
 ?>
 							<li> <a class="filter <?php echo " " . $quicklink[3]; if ($tplHelper->makeSelfUrl() == $tplHelper->makeBaseUrl() . $quicklink[2]) { echo " selected"; } ?>" href="<?php echo $quicklink[2]; ?>">
-							<img src='<?php echo $quicklink[1]; ?>' alt='<?php echo $quicklink[0]; ?>'><?php echo $quicklink[0]; if (stripos($quicklink[2], 'New:0')) { echo "<span class='newspots'>".$tplHelper->getNewCountForFilter($quicklink[2])."</span>"; } ?></a>
+							<img src='<?php echo $quicklink[1]; ?>' alt='<?php echo $quicklink[0]; ?>'><?php if (stripos($quicklink[2], 'New:0')) { echo "<span class='newspots'>".$tplHelper->getNewCountForFilter($quicklink[2])."</span>"; } echo $quicklink[0]; ?></a>
 <?php
     }
 ?>
@@ -61,7 +61,7 @@
 ?>
 						<li<?php if($filter[2]) { echo " class='". $tplHelper->filter2cat($filter[2]) ."'"; } ?>>
 						<a class="filter<?php echo " " . $filter[3]; if ($tplHelper->makeSelfUrl() == $strFilter) { echo " selected"; } ?>" href="<?php echo $strFilter;?>">
-						<img src='<?php echo $filter[1]; ?>' alt='<?php echo $filter[0]; ?>'><?php echo $filter[0]; echo "<span class='newspots'>$newCount</span>"; ?></a>
+						<img src='<?php echo $filter[1]; ?>' alt='<?php echo $filter[0]; ?>'><?php if ($newCount) { echo "<span class='newspots'>$newCount</span>"; } echo $filter[0]; ?></a>
 <?php
 		if (!empty($filter[4])) {
 			echo "\t\t\t\t\t\t\t<ul class='filterlist subfilterlist'>\r\n";
@@ -70,7 +70,7 @@
 				$newSubCount = $tplHelper->getNewCountForFilter($strFilter);
 ?>
 							<li> <a class="filter<?php echo " " . $subFilter[3]; if ($tplHelper->makeSelfUrl() == $strFilter) { echo " selected"; } ?>" href="<?php echo $strFilter;?>">
-							<img src='<?php echo $subFilter[1]; ?>' alt='<?php echo $subFilter[0]; ?>'><?php echo $subFilter[0]; echo "<span class='newspots'>$newSubCount</span>"; ?></a>
+							<img src='<?php echo $subFilter[1]; ?>' alt='<?php echo $subFilter[0]; ?>'><?php if ($newSubCount) { echo "<span class='newspots'>$newSubCount</span>"; } echo $subFilter[0]; ?></a>
 <?php
 				if (!empty($subFilter[4])) {
 					echo "\t\t\t\t\t\t\t<ul class='filterlist subfilterlist'>\r\n";
@@ -79,7 +79,7 @@
 						$newSub2Count = $tplHelper->getNewCountForFilter($strFilter);
 		?>
 							<li> <a class="filter<?php echo " " . $sub2Filter[3]; if ($tplHelper->makeSelfUrl() == $strFilter) { echo " selected"; } ?>" href="<?php echo $strFilter;?>">
-							<img src='<?php echo $sub2Filter[1]; ?>' alt='<?php echo $subFilter[0]; ?>'><?php echo $sub2Filter[0]; echo "<span class='newspots'>$newSub2Count</span>"; ?></a>
+							<img src='<?php echo $sub2Filter[1]; ?>' alt='<?php echo $subFilter[0]; ?>'><?php if ($newSub2Count) { echo "<span class='newspots'>$newSub2Count</span>"; } echo $sub2Filter[0]; ?></a>
 		<?php
 					} # foreach 
 					echo "\t\t\t\t\t\t\t</ul>\r\n";
