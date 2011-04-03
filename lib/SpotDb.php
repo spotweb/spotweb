a3565dcf92d577affde7595e00fe411570329c5b
rota <spotweb@hush.com>
2011-04-03 09:28:44 +0200

    sqlite ondersteuning butcherde de integer 0 waardes naar een lege string waardoor filters niet altijd goed werkten

--- lib/SpotDb.php
+++ lib/SpotDb.php
@@ -303,7 +303,6 @@ class SpotDb
 		} # if
 		
 		# en voer de query uit
-										 
  		return $this->_conn->arrayQuery("SELECT s.id AS id,
 												s.messageid AS messageid,
 												s.spotid AS spotid,
@@ -536,10 +535,10 @@ class SpotDb
 	 */
 	function addSpot($spot, $fullSpot = array()) {
 		$this->_conn->exec("INSERT INTO spots(spotid, messageid, category, subcat, poster, groupname, subcata, subcatb, subcatc, subcatd, title, tag, stamp, reversestamp) 
-				VALUES(%d, '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')",
+				VALUES(%d, '%s', %d, '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')",
 				 Array($spot['id'],
 					   $spot['messageid'],
-					   $spot['category'],
+					   (int) $spot['category'],
 					   $spot['subcat'],
 					   $spot['poster'],
 					   $spot['groupname'],
