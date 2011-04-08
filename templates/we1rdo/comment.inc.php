6d75f714fbcde89c6ff39d59a7ee7715989996aa
we1rdo <klaas248@gmail.com>
2011-04-08 15:58:04 +0200

    Fix voor odd/even probleem met background colors comments

--- templates/we1rdo/comment.inc.php
+++ templates/we1rdo/comment.inc.php
@@ -4,15 +4,14 @@
 	$pageNr = $tplHelper->getParam('pagenr');
 	
 	# we halen 5 spots per request op
-	$count = ($pageNr * 5);
 	$comments = $tplHelper->getSpotComments($messageId, ($pageNr * 5), 5);
 	$comments = $tplHelper->formatComments($comments);
 	
 	foreach($comments as $comment) {
 			if ($comment['verified']) {
 ?>
-					<li class="<?php $count++; echo ($count % 2 ? "odd" : "even"); ?>"> <strong> Gepost door <span class="user"><?php echo $comment['from']; ?></span> (<a class="userid" target = "_parent" href="<?php echo $tplHelper->makeUserIdUrl($comment); ?>" title='Zoek naar spots van "<?php echo $comment['from']; ?>"'><?php echo $comment['userid']; ?></a>) @ <?php echo $tplHelper->formatDate($comment['date'], 'comment'); ?> </strong> <br>
-						<?php echo join("<br>", $comment['body']); ?>
+					<li><strong> Gepost door <span class="user"><?php echo $comment['from']; ?></span> (<a class="userid" target = "_parent" href="<?php echo $tplHelper->makeUserIdUrl($comment); ?>" title='Zoek naar spots van "<?php echo $comment['from']; ?>"'><?php echo $comment['userid']; ?></a>) @ <?php echo $tplHelper->formatDate($comment['date'], 'comment'); ?> </strong> <br>
+						<?php echo utf8_encode(join("<br>", $comment['body'])); ?>
 					</li>
 <?php	
 			} # if
