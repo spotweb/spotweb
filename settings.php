07d8fd8f2c4a7f0befef21c2c0f7f6625fcd11ce
Nightspirit <ben@nightspirit.nl>
2011-04-09 20:51:30 +0200

    kleine wijzigingen

--- settings.php
+++ settings.php
@@ -279,6 +279,7 @@ if (!isset($settings['quicklinks'])) {
 	if ($settings['keep_downloadlist']) {
 		$settings['quicklinks'][] = Array('Gedownload', "images/icons/download.png", "?search[tree]=&amp;search[unfiltered]=true&amp;search[value][]=Downloaded:0", "");
 	}
+	$settings['quicklinks'][] = Array('Help', "images/icons/help.png", "?page=help", "");
 } # if isset
 
 #
