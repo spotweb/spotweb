20c18e82853462b3b9e2baf1f8f951c47a6dba4a
Nightspirit <ben@nightspirit.nl>
2011-04-14 01:10:43 +0200

    En dat verklaarde mijn dubbele indices

--- lib/dbstruct/SpotStruct_abs.php
+++ lib/dbstruct/SpotStruct_abs.php
@@ -44,14 +44,14 @@ abstract class SpotStruct_abs {
 		$this->addIndex("idx_spots_fts_1", "FULLTEXT", "spots", "title");
 		$this->addIndex("idx_spots_fts_2", "FULLTEXT", "spots", "poster");
 		$this->addIndex("idx_spots_fts_3", "FULLTEXT", "spots", "tag");
-		$this->addIndex("idx_spotsfull_fts_3", "FULLTEXT", "spotsfull", "userid");
+		$this->addIndex("idx_spotsfull_fts_1", "FULLTEXT", "spotsfull", "userid");
 		
 		# We voegen een reverse timestamp toe omdat MySQL MyISAM niet goed kan reverse sorteren 
 		if (!$this->columnExists('spots', 'reversestamp')) {
 			$this->addColumn("reversestamp", "spots", "INTEGER DEFAULT 0");
 			$this->_dbcon->rawExec("UPDATE spots SET reversestamp = (stamp*-1)");
 		} # if
-		$this->addIndex("idx_spots_6", "", "spots", "reversestamp");
+		$this->addIndex("idx_spots_3", "", "spots", "reversestamp");
 
 		# voeg de subcatz kolom toe zodat we hier in een type spot kunnen kenmerken
 		if (!$this->columnExists('spots', 'subcatz')) {
