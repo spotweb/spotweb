eebadb9ae2d1f1fdef61715f3eebd8b127a5237d
rota <spotweb@hush.com>
2011-04-15 11:30:19 +0200

    Fix

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 error_reporting(E_ALL & ~8192 & ~E_USER_WARNING);	# 8192 == E_DEPRECATED maar PHP < 5.3 heeft die niet
 session_start();
 
+require_once "lib/SpotClassAutoload.php";
 SpotTiming::start('total');
 SpotTiming::start('includes');
-require_once "lib/SpotClassAutoload.php";
 require_once "lib/SpotCookie.php";
 require_once "settings.php";
 SpotTiming::stop('includes');
