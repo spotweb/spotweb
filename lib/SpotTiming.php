26276d0207e98f20ee6d3d26ffa758a7eb98203d
rota <spotweb@hush.com>
2011-04-15 08:53:48 +0200

    En nu met spottiming.php

---
+++ lib/SpotTiming.php
@@ -0,0 +1,43 @@
+<?php
+
+class SpotTiming {
+	static private $_enabled = true;
+	static private $_timings = array();
+	static private $_inflight = array();
+	static private $_curlevel = 0;
+	
+	static function disable() {
+		self::$_enabled = false;
+	} # enable
+	
+	static function start($name) {
+		if (!self::$_enabled) return;
+		
+		self::$_curlevel++;
+		self::$_inflight[$name] = array('start' => microtime(true));
+	} # start
+	
+	static function stop($name, $extra = '') {
+		if (!self::$_enabled) return;
+
+		self::$_inflight[$name]['stop'] = microtime(true);
+		self::$_inflight[$name]['extra'] = $extra;
+		self::$_inflight[$name]['level'] = self::$_curlevel;
+		self::$_curlevel--;
+
+		self::$_timings[] = array_merge(self::$_inflight[$name], array('name' => $name));
+		unset(self::$_inflight[$name]);
+	} # stop
+	
+	static function display() {
+		if (!self::$_enabled) return;
+		
+		echo '<table style="border: 1px solid black; border-collapse: collapse;" border=1><tr><th>Name</th><th>Time</th><th>Extra</th></tr>';
+		
+		foreach(array_reverse(self::$_timings) as $values) {
+			echo '<tr><td>' . str_pad('', $values['level'], '.') . $values['name'] . '</td><td>' . ($values['stop'] - $values['start']) . '</td><td>' . serialize($values['extra']) . '</td></tr>' . PHP_EOL;
+		} # foreach
+	
+		echo '</table><br><br><br><br>';
+	} # display()
+} # class SpotTiming
