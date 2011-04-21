215fe0c81eb866e3d10f2c8424db8871bdeb7e48
we1rdo <klaas248@gmail.com>
2011-04-21 23:45:53 +0200

    user systeem implementatie verder geimplementeerd (createUser werkt nu volledig)

--- templates/we1rdo/createuser.inc.php
+++ templates/we1rdo/createuser.inc.php
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-<?php include "includes/form-messages.inc.php"; ?>
-   
-<form class="createuserform" name="createuserform" action="<?php echo $tplHelper->makeCreateUserAction(); ?>" method="post">
-=======
 <?php 
 
 if ((!empty($createresult)) || (!empty($formmessages))) {
@@ -15,34 +10,26 @@ if (empty($createresult)) {
 	include "includes/form-messages.inc.php";
 
 ?>
-<form name="createuserform" action="<?php echo $tplHelper->makeCreateUserAction(); ?>" method="post">
->>>>>>> upstream/master
-<input type="hidden" name="createuserform[xsrfid]" value="<?php echo $tplHelper->generateXsrfCookie('createuserform'); ?>">
+<form class="createuserform" name="createuserform" action="<?php echo $tplHelper->makeCreateUserAction(); ?>" method="post">
+	<input type="hidden" name="createuserform[xsrfid]" value="<?php echo $tplHelper->generateXsrfCookie('createuserform'); ?>">
     <fieldset>
         <dl>
-            <dt><label for="createuserform[username]">Username</label></dt>
+            <dt><label for="createuserform[username]">Gebruikersnaam</label></dt>
             <dd><input type="text" name="createuserform[username]" value="<?php echo htmlspecialchars($createuserform['username']); ?>"></dd>
 
-            <dt><label for="createuserform[firstname]">First name</label></dt>
+            <dt><label for="createuserform[firstname]">Voornaam</label></dt>
             <dd><input type="text" name="createuserform[firstname]" value="<?php echo htmlspecialchars($createuserform['firstname']); ?>"></dd>
 
-            <dt><label for="createuserform[lastname]">Last name</label></dt>
+            <dt><label for="createuserform[lastname]">Achternaam</label></dt>
             <dd><input type="text" name="createuserform[lastname]"  value="<?php echo htmlspecialchars($createuserform['lastname']); ?>"></dd>
             
-            <dt><label for="createuserform[mail]">Mailaddress</label></dt>
+            <dt><label for="createuserform[mail]">Mailadres</label></dt>
             <dd><input type="text" name="createuserform[mail]"  value="<?php echo htmlspecialchars($createuserform['mail']); ?>"></dd>
 
-<<<<<<< HEAD
-            <dd><input class="greyButton" type="submit" name="createuserform[submit]" value="Toevoegen"></dd>
-        </dl>
-    </fieldset>
-</form>
-=======
-			<dd><input type="submit" name="createuserform[submit]" value="Add"></dd>
+			<dd><input class="greyButton" type="submit" name="createuserform[submit]" value="Toevoegen"></dd>
 		</dl>
 	</fieldset>
 </form>
 <?php
 	}
 ?>
->>>>>>> upstream/master
