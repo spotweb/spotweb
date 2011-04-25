7853dd3a720033ce8e7fa1d54fc17f24a42dc81a
rota <spotweb@hush.com>
2011-04-25 09:22:33 +0200

    Verdere implementatie van posting, zou nu kunnen werken mits de NNTP class niet buggy was

--- lib/SpotNntp.php
+++ lib/SpotNntp.php
@@ -176,34 +176,29 @@ class SpotNntp {
 			return $comments;
 		} # getComments
 
-		function postComment($user, $hashCash, $serverPrivKey, $rating, $newsgroup, $inReplyTo, $title, $content) {
+		function postComment($user, $serverPrivKey, $newsgroup, $title, $comment) {
 			# FIXME: Het aantal nullen (minimaal 4) instelbaar maken via settings.php
-		
-			# We genereren een uniek messageid dat ook nog eens als eerste vier bytes 0000 geeft
-			# van een SHA1 hash. 
-			
-			# Rating is 1 t/m 10, en bevat een rating van de spot waar dit commentaar over gaat
-			# Spotweb zou in het overzicht bij elke spot de gemiddelde beoordeling kunnen tonen.
-			$newMessageId = $spotSigning->makeExpensiveHash("<" . $inReplyTo . "." . $rating . "." . 
-								$spotParser->specialString(base64_encode($spotSigning->makeRandomStr(4))), 
-								"@spot.net>");
-			
-			# en sign het messageid
-			$user_signature = $spotSigning->signMessage($user['privatekey'], $newMessageId);
+
+			# instantieer de benodigde objecten
+			$spotSigning = new SpotSigning();
+			$spotParser = new SpotParser();
+
+			# sign het messageid
+			$user_signature = $spotSigning->signMessage($user['privatekey'], $comment['newmessageid']);
 			
 			# ook door de php server 
-			$server_signature = $spotSigning->signMessage($serverPrivKey, $newMessageId);
-			
-			$header = 'From: ' . $user['name'] . " <" . trim($user['name']) . '@spot.net>' . "\r\n";
+			$server_signature = $spotSigning->signMessage($serverPrivKey, $comment['newmessageid']);
+
+			$header = 'From: ' . $user['username'] . " <" . trim($user['username']) . '@spot.net>' . "\r\n";
 			$header .= 'Subject: Re: ' . $title . "\r\n";
 			$header .= 'Newsgroups: ' . $newsgroup . "\r\n";
-			$header .= 'Message-ID: ' . $newMessageId . "\r\n";
-			$header .= 'References: <' . $inReplyTo. ">\r\n";
+			$header .= 'Message-ID: ' . $comment['newmessageid'] . "\r\n";
+			$header .= 'References: <' . $comment['inreplyto']. ">\r\n";
 			$header .= 'X-User-Signature: ' . $spotParser->specialString($user_signature['signature']) . "\r\n";
 			$header .= 'X-Server-Signature: ' . $spotParser->specialString($server_signature['signature']) . "\r\n";
 			$header .= 'X-User-Key: ' . $spotSigning->pubkeyToXml($user_signature['publickey']) . "\r\n";
 			$header .= 'X-Server-Key: ' . $spotSigning->pubkeyToXml($server_signature['publickey']) . "\r\n";
-			$header .= 'X-User-Rating: ' . $rating . "\r\n";
+			$header .= 'X-User-Rating: ' . (int) $comment['rating'] . "\r\n";
 			
 			# $header .= 'X-User-Avatar: ' 
 			# Message-ID van een avatar
@@ -214,8 +209,7 @@ class SpotNntp {
 			$header .= "X-Newsreader: SpotWeb 0.9\r\n";
 			$header .= "X-No-Archive: yes\r\n";
 			
-			return $this->post(array($header, $content));
-
+			return $this->post(array($header, $comment['body']));
 		} # postComment
 		
 		function getImage($segment) {
--- lib/SpotPosting.php
+++ lib/SpotPosting.php
@@ -16,35 +16,42 @@ class SpotPosting {
 	 * Post een comment op een spot naar de newsserver, als dit lukt komt er
 	 * een 'true' terug, anders een foutmelding
 	 */
-	public function postComment($user, $body, $rating, $inReplyTo, $newMessageId) {
+	public function postComment($user, $comment) {
 		# haal de spot op waar dit een reply op is
 		$spotsOverview = new SpotsOverview($this->_db, $this->_settings);
-		$fullSpot = $spotsOverview->getFullSpot($inReplyTo, $user['userid'], $this->_nntp_post);
+		$fullSpot = $spotsOverview->getFullSpot($comment['inreplyto'], $user['userid'], $this->_nntp_post);
 
+		var_dump($comment);
+		
 		# als de hashcash al niet klopt, doen we verder geen moeite
-		if (substr(sha1($newMessageId), 0, 4) != '0000') {
-			return array('errors' => array('invalidhashcash'));
+		if (substr(sha1('<' . $comment['newmessageid'] . '>'), 0, 4) != '0000') {
+			return array('errors' => array('postcomment_invalidhashcash'));
 		} # if
 
 		# controleer dat de messageid waarop we replyen overeenkomt
 		# met het newMessageid om replay-attacks te voorkomen.
-		// FIXME
+		$replyToPart = substr($comment['inreplyto'], 0, strpos($comment['inreplyto'], '@'));
+
+		if (substr($comment['newmessageid'], 0, strlen($replyToPart)) != $replyToPart) { 
+			return array('errors' => array('postcomment_replayattack'));
+		} # if
 		
 		# controleer dat het random getal niet recentelijk ook al gebruikt
 		# is voor deze messageid (hiermee voorkomen we dat de hashcash niet
 		# steeds herberekend wordt voor het volspammen van 1 spot).
-		// FIXME
-echo 'alles ok, we gaan posten!';
-die();
-		
+		if (!$this->_db->isCommentMessageIdUnique($comment['newmessageid'])) {
+			return array('errors' => array('postcomment_replayattack'));
+		} # if
+
 		# en post daadwerkelijk de comment
-		$this->_nntp_post->postComment($this->_currentSession['user'],
+		$this->_nntp_post->postComment($user,
 									   $this->_settings->get('privatekey'),  # Server private key
-									   $rating,
-									   $settings->get('comment_group'),
-									   $inReplyTo, 
+									   $this->_settings->get('comment_group'),
 									   $fullSpot['title'], 
-									   $body);
+									   $comment);
+		$this->_db->addPostedComment($user['userid'], $comment);
+		
+		return array();
 	} # postComment
 	
 } # SpotPosting
