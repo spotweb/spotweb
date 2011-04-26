93fe715e90ec453161916111357c6a9980f09460
nightspirit81 <ben@nightspirit.nl>
2011-04-26 20:57:01 +0200

    - Eigen API key geïntroduceerd - Volledige $settings wordt nu doorgegeven aan SpotPage_sabapi - $settings['nzbhandling']['sabnzbd']['url'] wordt nu ook gebruikt (handig als iemand liever https gebruikt of een "aparte" sab-installatie heeft)

--- lib/page/SpotPage_sabapi.php
+++ lib/page/SpotPage_sabapi.php
@@ -7,9 +7,19 @@ class SpotPage_sabapi extends SpotPage_Abs {
 	} # __ctor
 
 	function render() {
-		if ($this->_settings['action'] != "push-sabnzbd") {
+		$SpotUserSystem = new SpotUserSystem($this->_dbsettings, $this->_settings);
+
+		parse_str($_SERVER['QUERY_STRING'], $request);
+		$nzbhandling = $this->_settings->get('nzbhandling');
+		$sabnzbd = $nzbhandling['sabnzbd'];
+
+		if ($nzbhandling['action'] != "push-sabnzbd") {
 			die ("SABzndb is not configured on this node.");
-		} # if
+		} elseif (!isset($request['apikey'])) {
+			die ("API Key Required");
+		} elseif ($SpotUserSystem->passToHash($sabnzbd['apikey']) != $request['apikey']) {
+			die ("API Key Incorrect");
+		} # else
 
 		header('Cache-Control: no-cache, must-revalidate');
 		header('Expires: Mon, 15 Apr 2006 12:26:00 GMT');
@@ -20,11 +30,20 @@ class SpotPage_sabapi extends SpotPage_Abs {
 			header('Content-type: application/json');
 		} # else
 
-		$request = str_replace("page=sabapi&", "", $_SERVER['QUERY_STRING']);
-		$url = "http://" . $this->_settings['sabnzbd']['host'] . "/sabnzbd/api?" . $request;
+		$apicall = str_replace("page=sabapi&", "", $_SERVER['QUERY_STRING']);
+		$apicall = array();
+		foreach($request as $key => $value) {
+			if ($key != "page" && $key != "apikey")
+			$apicall[] = $key . "=" . $value;
+		}
+		$request = implode("&", $apicall);
+		
+		$this->_url = parse_url($sabnzbd['url']);
+		$this->_url = str_replace('$SABNZBDHOST', $sabnzbd['host'], $this->_url);
+
+		$url = $this->_url['scheme'] . "://" . $this->_url['host'] . $this->_url['path'] . "?" . $request . "&apikey=" . $sabnzbd['apikey'];
 		$output = @file_get_contents($url, 0);
 		echo $output;
-
 	} # render
 
 } # class SpotPage_sabapi
