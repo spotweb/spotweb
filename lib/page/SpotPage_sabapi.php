d4ae1ebfcdf2855ff3f86a42f8e86fd7482dec18
nightspirit81 <ben@nightspirit.nl>
2011-04-26 21:02:53 +0200

    Consequenter quotes gebruikt

--- lib/page/SpotPage_sabapi.php
+++ lib/page/SpotPage_sabapi.php
@@ -13,35 +13,35 @@ class SpotPage_sabapi extends SpotPage_Abs {
 		$nzbhandling = $this->_settings->get('nzbhandling');
 		$sabnzbd = $nzbhandling['sabnzbd'];
 
-		if ($nzbhandling['action'] != "push-sabnzbd") {
-			die ("SABzndb is not configured on this node.");
+		if ($nzbhandling['action'] != 'push-sabnzbd') {
+			die ('SABzndb is not configured on this node.');
 		} elseif (!isset($request['apikey'])) {
-			die ("API Key Required");
+			die ('API Key Required');
 		} elseif ($SpotUserSystem->passToHash($sabnzbd['apikey']) != $request['apikey']) {
-			die ("API Key Incorrect");
+			die ('API Key Incorrect');
 		} # else
 
 		header('Cache-Control: no-cache, must-revalidate');
 		header('Expires: Mon, 15 Apr 2006 12:26:00 GMT');
 
 		if (stripos($_SERVER['QUERY_STRING'], 'output=xml')) {
-			header("Content-Type:text/xml");
+			header('Content-Type:text/xml');
 		} elseif (stripos($_SERVER['QUERY_STRING'], 'output=json')) {
 			header('Content-type: application/json');
 		} # else
 
-		$apicall = str_replace("page=sabapi&", "", $_SERVER['QUERY_STRING']);
+		$apicall = str_replace('page=sabapi&', '', $_SERVER['QUERY_STRING']);
 		$apicall = array();
 		foreach($request as $key => $value) {
-			if ($key != "page" && $key != "apikey")
-			$apicall[] = $key . "=" . $value;
+			if ($key != 'page' && $key != 'apikey')
+			$apicall[] = $key . '=' . $value;
 		}
-		$request = implode("&", $apicall);
+		$request = implode('&', $apicall);
 		
 		$this->_url = parse_url($sabnzbd['url']);
 		$this->_url = str_replace('$SABNZBDHOST', $sabnzbd['host'], $this->_url);
 
-		$url = $this->_url['scheme'] . "://" . $this->_url['host'] . $this->_url['path'] . "?" . $request . "&apikey=" . $sabnzbd['apikey'];
+		$url = $this->_url['scheme'] . '://' . $this->_url['host'] . $this->_url['path'] . '?' . $request . '&apikey=' . $sabnzbd['apikey'];
 		$output = @file_get_contents($url, 0);
 		echo $output;
 	} # render
