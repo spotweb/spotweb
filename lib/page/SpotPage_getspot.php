f74b7d04287af943b02c3eb36bd5779d0d699ac3
nightspirit81 <ben@nightspirit.nl>
2011-04-28 05:22:56 +0200

    Vermoederlijk bugje opgelost in SpotPage_getspot.php

--- lib/page/SpotPage_getspot.php
+++ lib/page/SpotPage_getspot.php
@@ -1,35 +1,38 @@
 <?php
 class SpotPage_getspot extends SpotPage_Abs {
 	private $_messageid;
-	
+
 	function __construct(SpotDb $db, SpotSettings $settings, $currentSession, $messageid) {
 		parent::__construct($db, $settings, $currentSession);
 		$this->_messageid = $messageid;
 	} # ctor
 
-
 	function render() {
 		$spotnntp = new SpotNntp($this->_settings->get('nntp_hdr'));
-		
+
 		# Haal de volledige spotinhoud op
 		$spotsOverview = new SpotsOverview($this->_db, $this->_settings);
 		$fullSpot = $spotsOverview->getFullSpot($this->_messageid, $this->_currentSession['user']['userid'], $spotnntp);
 
 		# zet de page title
 		$this->_pageTitle = "spot: " . $fullSpot['title'];
-		
+
 		# seen list
-		if ($this->_settings->get('count_newspots') && $this->_settings->get('keep_seenlist') && $fullSpot['seenstamp'] == NULL && (
-		  $this->_settings->get('auto_markasread') && max($this->_currentSession['user']['lastvisit'],$this->_currentSession['user']['lastseen']) < $fullSpot['stamp'] ||
-		  $this->_currentSession['user']['lastseen'] < $fullSpot['stamp'])
-		  ) {
-			$spotsOverview->addToSeenList($this->_messageid, $this->_currentSession['user']['userid']);
+		if (	$this->_settings->get('count_newspots')
+				&& $this->_settings->get('keep_seenlist')
+				&& $fullSpot['seenstamp'] == NULL
+					&& (
+						( $this->_settings->get('auto_markasread') && max($this->_currentSession['user']['lastvisit'],$this->_currentSession['user']['lastseen']) < $fullSpot['stamp'] )
+						|| $this->_currentSession['user']['lastseen'] < $fullSpot['stamp']
+					)
+			) {
+				$spotsOverview->addToSeenList($this->_messageid, $this->_currentSession['user']['userid']);
 		} # if
-		
+
 		#- display stuff -#
 		$this->template('header');
 		$this->template('spotinfo', array('spot' => $fullSpot));
 		$this->template('footer');
 	} # render
-	
+
 } # class SpotPage_getspot
