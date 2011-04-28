215d6329fb3e8ce55823616a2852bbb554f1fff3
rota <spotweb@hush.com>
2011-04-28 22:31:00 +0200

    Blobs/text velden mogen geen default value hbben, fixes #610

--- lib/dbstruct/SpotStruct_abs.php
+++ lib/dbstruct/SpotStruct_abs.php
@@ -184,7 +184,7 @@ abstract class SpotStruct_abs {
 			$this->_dbcon->rawExec("ALTER TABLE spotsfull MODIFY fullxml TEXT CHARACTER SET utf8");
 
 			$this->_dbcon->rawExec("ALTER TABLE watchlist MODIFY messageid VARCHAR(128) CHARACTER SET ascii DEFAULT ''  NOT NULL");
-			$this->_dbcon->rawExec("ALTER TABLE watchlist MODIFY comment TEXT CHARACTER SET utf8 DEFAULT '' NOT NULL");
+			$this->_dbcon->rawExec("ALTER TABLE watchlist MODIFY comment TEXT CHARACTER SET utf8 NOT NULL");
 
 			echo "Dropping indexes (5/10)" . PHP_EOL;
 
