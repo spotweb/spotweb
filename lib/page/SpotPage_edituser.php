d549ab98f3f328d52b6c4c9cc74f0215cdb906c0
rota <spotweb@hush.com>
2011-05-01 09:34:26 +0200

    Edituser toegevoegd

---
+++ lib/page/SpotPage_edituser.php
@@ -0,0 +1,63 @@
+<?php
+class SpotPage_edituser extends SpotPage_Abs {
+	private $_editUserForm;
+	private $_userIdToEdit;
+	
+	function __construct(SpotDb $db, SpotSettings $settings, $currentSession, $params) {
+		parent::__construct($db, $settings, $currentSession);
+		$this->_editUserForm = $params['edituserform'];
+		$this->_userIdToEdit = $params['userid'];
+	} # ctor
+
+	function render() {
+		$formMessages = array('errors' => array(),
+							  'info' => array());
+							  
+		# edituser resultaat is standaard niet geprobeerd
+		$editResult = array();
+
+		# Instantieer het Spot user system
+		$spotUserSystem = new SpotUserSystem($this->_db, $this->_settings);
+		
+		# zet de page title
+		$this->_pageTitle = "spot: edit user";
+		
+		# haal de te editten user op 
+		$spotUser = $this->_db->getUser($this->_userIdToEdit);
+		if ($spotUser === false) {
+			$formMessages['errors'][] = array('edituser_usernotfound', array($spotUser['username']));
+			$editResult = array('result' => 'failure');
+		} # if
+		
+		# Is dit een submit van een form, of nog maar de aanroep?
+		if (isset($this->_editUserForm['submit']) && (empty($formMessages['errors']))) {
+			# submit unsetten we altijd
+			unset($this->_editUserForm['submit']);
+			
+			# valideer de user
+			$spotUser = array_merge($spotUser, $this->_editUserForm);
+			$formMessages['errors'] = $spotUserSystem->validateUserRecord($spotUser);
+			
+			if (empty($formMessages['errors'])) {
+				# voeg de user toe
+				$spotUserSystem->setUser($spotUser);
+				
+				# als de gebruker een nieuw wachtwoord opgegeven heeft, update dan 
+				# het wachtwoord ook
+				$spotUserSystem->setUserPassword($spotUser);
+				
+				# als het toevoegen van de user gelukt is, laat het weten
+				$editResult = array('result' => 'success');
+			} else {
+				$editResult = array('result' => 'failure');
+			} # else
+			
+		} # if
+		
+		#- display stuff -#
+		$this->template('edituser', array('edituserform' => $spotUser,
+										    'formmessages' => $formMessages,
+											'editresult' => $editResult));
+	} # render
+	
+} # class SpotPage_edituser
---
+++ templates/we1rdo/edituser.inc.php
@@ -0,0 +1,42 @@
+<?php 
+
+if (!empty($createresult)) {
+	include 'includes/form-xmlresult.inc.php';
+	
+	echo formResult2Xml($createresult, $formmessages, $tplHelper);
+} # if
+
+if (empty($createresult)) {
+	include "includes/form-messages.inc.php";
+
+?>
+<form class="edituserform" name="edituserform" action="<?php echo $tplHelper->makeEditUserAction(); ?>" method="post">
+	<input type="hidden" name="edituserform[xsrfid]" value="<?php echo $tplHelper->generateXsrfCookie('edituserform'); ?>">
+	<input type="hidden" name="userid" value="<?php echo $edituserform['userid']; ?>">
+    <fieldset>
+        <dl>
+            <dt><label for="edituserform[username]">Gebruikersnaam</label></dt>
+            <dd><?php echo htmlspecialchars($edituserform['username']); ?></dd>
+
+            <dt><label for="edituserform[username]">Wachtwoord</label></dt>
+            <dd><input type="text" name="edituserform[newpassword1]" value=""></dd>
+
+            <dt><label for="edituserform[username]">Wachtwoord (bevestig)</label></dt>
+            <dd><input type="text" name="edituserform[newpassword2]" value=""></dd>
+
+            <dt><label for="edituserform[firstname]">Voornaam</label></dt>
+            <dd><input type="text" name="edituserform[firstname]" value="<?php echo htmlspecialchars($edituserform['firstname']); ?>"></dd>
+
+            <dt><label for="edituserform[lastname]">Achternaam</label></dt>
+            <dd><input type="text" name="edituserform[lastname]"  value="<?php echo htmlspecialchars($edituserform['lastname']); ?>"></dd>
+            
+            <dt><label for="edituserform[mail]">Mailadres</label></dt>
+            <dd><input type="text" name="edituserform[mail]"  value="<?php echo htmlspecialchars($edituserform['mail']); ?>"></dd>
+
+			<dd><input class="greyButton" type="submit" name="edituserform[submit]" value="Bijwerken"></dd>
+		</dl>
+	</fieldset>
+</form>
+<?php
+	}
+?>
