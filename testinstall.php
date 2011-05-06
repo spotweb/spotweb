80ece7c870dc5aa37fa210029225b5e58373fe67
nightspirit81 <ben@nightspirit.nl>
2011-05-06 18:53:30 +0200

    Correcte benaming aan Net_NNTP gegeven

--- testinstall.php
+++ testinstall.php
@@ -49,9 +49,9 @@
 		<tr> <td> Own settings file </td> <td> <?php echo showResult(testInclude("ownsettings.php"), testInclude("ownsettings.php"), "optioneel"); ?>  </td> </tr>
 		<tr> <td> PEAR </td> <td> <?php echo showResult(testInclude("System.php"), testInclude("System.php")); ?>  </td> </tr>
 		<?php if (PHP_OS == "WIN32" || PHP_OS == "WINNT") { ?>
-			<tr> <td> PEAR Net/NNTP </td> <td> <?php echo showResult(testInclude("Net\\NNTP\\Client.php"), testInclude("Net\\NNTP\\Client.php")); ?>  </td> </tr>
+			<tr> <td> PEAR Net_NNTP </td> <td> <?php echo showResult(testInclude("Net\\NNTP\\Client.php"), testInclude("Net\\NNTP\\Client.php")); ?>  </td> </tr>
 		<?php } else { ?>
-			<tr> <td> PEAR Net/NNTP </td> <td> <?php echo showResult(testInclude("Net/NNTP/Client.php"), testInclude("Net/NNTP/Client.php")); ?>  </td> </tr>
+			<tr> <td> PEAR Net_NNTP </td> <td> <?php echo showResult(testInclude("Net/NNTP/Client.php"), testInclude("Net/NNTP/Client.php")); ?>  </td> </tr>
 		<?php } ?>
 		<tr> <td> NNTP server </td> <td> <?php showResult(empty($settings['nntp_nzb']['host']) === false, $settings['nntp_nzb']['host'], "No server entered"); ?>  </td> </tr>
 	</table>
