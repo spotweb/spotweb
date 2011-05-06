e6ca1f51677223c364cc933de7ad4537decab0ba
nightspirit81 <ben@nightspirit.nl>
2011-05-06 18:43:49 +0200

    Fix #637

--- testinstall.php
+++ testinstall.php
@@ -48,7 +48,11 @@
 		<tr> <td> Settings file </td> <td> <?php echo showResult(testInclude("settings.php"), testInclude("settings.php")); ?>  </td> </tr>
 		<tr> <td> Own settings file </td> <td> <?php echo showResult(testInclude("ownsettings.php"), testInclude("ownsettings.php"), "optioneel"); ?>  </td> </tr>
 		<tr> <td> PEAR </td> <td> <?php echo showResult(testInclude("System.php"), testInclude("System.php")); ?>  </td> </tr>
-		<tr> <td> PEAR Net/NNTP </td> <td> <?php echo showResult(testInclude("Net/NNTP/Client.php"), testInclude("Net/NNTP/Client.php")); ?>  </td> </tr>
+		<?php if (PHP_OS == "WIN32" || PHP_OS == "WINNT") { ?>
+			<tr> <td> PEAR Net/NNTP </td> <td> <?php echo showResult(testInclude("Net\NNTP\Client.php"), testInclude("Net\NNTP\Client.php")); ?>  </td> </tr>
+		<?php } else { ?>
+			<tr> <td> PEAR Net/NNTP </td> <td> <?php echo showResult(testInclude("Net/NNTP/Client.php"), testInclude("Net/NNTP/Client.php")); ?>  </td> </tr>
+		<?php } ?>
 		<tr> <td> NNTP server </td> <td> <?php showResult(empty($settings['nntp_nzb']['host']) === false, $settings['nntp_nzb']['host'], "No server entered"); ?>  </td> </tr>
 	</table>
 
