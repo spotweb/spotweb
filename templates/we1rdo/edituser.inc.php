f9557f66a3316c774b3dbe0c103559789f64544f
nightspirit81 <ben@nightspirit.nl>
2011-05-08 19:53:38 +0200

    Wachtwoorden horen niet zichtbaar op het scherm

--- templates/we1rdo/edituser.inc.php
+++ templates/we1rdo/edituser.inc.php
@@ -20,10 +20,10 @@ if (empty($editresult)) {
             <dd><?php echo htmlspecialchars($edituserform['username']); ?></dd>
 
             <dt><label for="edituserform[newpassword1]">Wachtwoord</label></dt>
-            <dd><input type="text" name="edituserform[newpassword1]" value=""></dd>
+            <dd><input type="password" name="edituserform[newpassword1]" value=""></dd>
 
             <dt><label for="edituserform[newpassword2]">Wachtwoord (bevestig)</label></dt>
-            <dd><input type="text" name="edituserform[newpassword2]" value=""></dd>
+            <dd><input type="password" name="edituserform[newpassword2]" value=""></dd>
 
             <dt><label for="edituserform[firstname]">Voornaam</label></dt>
             <dd><input type="text" name="edituserform[firstname]" value="<?php echo htmlspecialchars($edituserform['firstname']); ?>"></dd>
