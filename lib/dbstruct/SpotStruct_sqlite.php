c6d901d9a9f2a61f7b1faf2228a1e2f1f5eca448
nightspirit81 <ben@nightspirit.nl>
2011-05-14 14:58:29 +0200

    RFC 1036 toegepast

--- lib/dbstruct/SpotStruct_sqlite.php
+++ lib/dbstruct/SpotStruct_sqlite.php
@@ -62,8 +62,8 @@ class SpotStruct_sqlite extends SpotStruct_abs {
 										   messageid VARCHAR(128),
 										   nntpref VARCHAR(128),
 										   spotrating INTEGER DEFAULT 0);");
-			$this->_dbcon->rawExec("CREATE UNIQUE INDEX idx_commentsxover_1 ON commentsxover(nntpref, messageid)");
-			$this->_dbcon->rawExec("CREATE UNIQUE INDEX idx_commentsxover_2 ON commentsxover(messageid)");
+			$this->_dbcon->rawExec("CREATE UNIQUE INDEX idx_commentsxover_1 ON commentsxover(messageid)");
+			$this->_dbcon->rawExec("CREATE INDEX idx_commentsxover_2 ON commentsxover(nntpref)");
 		} # if
 			
 		# spotstatelist table
