e23227e907d18776254a83192aa18894051b4030
nightspirit81 <ben@nightspirit.nl>
2011-05-22 02:57:33 +0200

    RSS link hoeft geen standaard sorting mee te krijgenm maar erft deze van de opgevraagde link

--- templates/we1rdo/includes/header.inc.php
+++ templates/we1rdo/includes/header.inc.php
@@ -6,7 +6,7 @@
 <?php if ($settings->get('deny_robots')) { echo "\t\t<meta name=\"robots\" content=\"noindex, nofollow\">\r\n"; } ?>
 		<base href='<?php echo $tplHelper->makeBaseUrl("full"); ?>'>
 		<link rel='stylesheet' type='text/css' href='?page=statics&amp;type=css&amp;mod=<?php echo $tplHelper->getStaticModTime('css'); ?>'>
-		<link rel='alternate' type='application/rss+xml' href='<?php echo $tplHelper->getPageUrl('rss', true) . "&amp;sortby=stamp&amp;sortdir=DESC" . $tplHelper->makeApiRequestString(); ?>'>
+		<link rel='alternate' type='application/rss+xml' href='<?php echo $tplHelper->getPageUrl('rss', true) . $tplHelper->makeApiRequestString(); ?>'>
 		<link rel='shortcut icon' href='?page=statics&amp;type=ico&amp;mod=<?php echo $tplHelper->getStaticModTime('ico'); ?>'>
 		<script src='?page=statics&amp;type=js&amp;mod=<?php echo $tplHelper->getStaticModTime('js'); ?>' type='text/javascript'></script>
 		<script type='text/javascript'>
