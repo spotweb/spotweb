1ee1516f186567ab781b60a3d303049c3df97b3b
rota <spotweb@hush.com>
2011-05-29 07:29:56 +0200

    View RSS feed in de UI verwerkt

--- templates/we1rdo/includes/header.inc.php
+++ templates/we1rdo/includes/header.inc.php
@@ -6,7 +6,9 @@
 <?php if ($settings->get('deny_robots')) { echo "\t\t<meta name=\"robots\" content=\"noindex, nofollow\">\r\n"; } ?>
 		<base href='<?php echo $tplHelper->makeBaseUrl("full"); ?>'>
 		<link rel='stylesheet' type='text/css' href='?page=statics&amp;type=css&amp;mod=<?php echo $tplHelper->getStaticModTime('css'); ?>'>
+<?php if ($tplHelper->allowed(SpotSecurity::spotsec_view_rssfeed, '')) { ?>
 		<link rel='alternate' type='application/rss+xml' href='<?php echo $tplHelper->getPageUrl('rss', true) . $tplHelper->makeApiRequestString(); ?>'>
+<?php } ?>
 		<link rel='shortcut icon' href='?page=statics&amp;type=ico&amp;mod=<?php echo $tplHelper->getStaticModTime('ico'); ?>'>
 		<script src='?page=statics&amp;type=js&amp;mod=<?php echo $tplHelper->getStaticModTime('js'); ?>' type='text/javascript'></script>
 		<script type='text/javascript'>
