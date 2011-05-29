d9323edd679bbad49f8918d74f91fdb7b8b34dd8
rota <spotweb@hush.com>
2011-05-29 07:21:24 +0200

    spotsec_view_spotdetail verwerkt in de Ui

--- templates/we1rdo/includes/header.inc.php
+++ templates/we1rdo/includes/header.inc.php
@@ -9,6 +9,11 @@
 		<link rel='alternate' type='application/rss+xml' href='<?php echo $tplHelper->getPageUrl('rss', true) . $tplHelper->makeApiRequestString(); ?>'>
 		<link rel='shortcut icon' href='?page=statics&amp;type=ico&amp;mod=<?php echo $tplHelper->getStaticModTime('ico'); ?>'>
 		<script src='?page=statics&amp;type=js&amp;mod=<?php echo $tplHelper->getStaticModTime('js'); ?>' type='text/javascript'></script>
+		<script type='text/javascript'>
+			// Define some global variables showing or hiding specific parts of the UI
+			// based on users' security rights
+			var spotweb_security_allow_spotdetail = <?php echo (int) $tplHelper->allowed(SpotSecurity::spotsec_view_spotdetail, ''); ?>;
+		</script>
 	</head>
 	<body>
 		<div id="overlay"></div>
