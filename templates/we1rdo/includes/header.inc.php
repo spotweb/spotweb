f80872f74eb6f2045689e033c986e4ba76fd8798
rota <spotweb@hush.com>
2011-05-29 07:30:26 +0200

    spotsec_view_statics in de UI verwerkt

--- templates/we1rdo/includes/header.inc.php
+++ templates/we1rdo/includes/header.inc.php
@@ -5,12 +5,14 @@
 		<title><?php echo $pagetitle?></title>
 <?php if ($settings->get('deny_robots')) { echo "\t\t<meta name=\"robots\" content=\"noindex, nofollow\">\r\n"; } ?>
 		<base href='<?php echo $tplHelper->makeBaseUrl("full"); ?>'>
-		<link rel='stylesheet' type='text/css' href='?page=statics&amp;type=css&amp;mod=<?php echo $tplHelper->getStaticModTime('css'); ?>'>
 <?php if ($tplHelper->allowed(SpotSecurity::spotsec_view_rssfeed, '')) { ?>
 		<link rel='alternate' type='application/rss+xml' href='<?php echo $tplHelper->getPageUrl('rss', true) . $tplHelper->makeApiRequestString(); ?>'>
 <?php } ?>
+<?php if ($tplHelper->allowed(SpotSecurity::spotsec_view_statics, '')) { ?>
+		<link rel='stylesheet' type='text/css' href='?page=statics&amp;type=css&amp;mod=<?php echo $tplHelper->getStaticModTime('css'); ?>'>
 		<link rel='shortcut icon' href='?page=statics&amp;type=ico&amp;mod=<?php echo $tplHelper->getStaticModTime('ico'); ?>'>
 		<script src='?page=statics&amp;type=js&amp;mod=<?php echo $tplHelper->getStaticModTime('js'); ?>' type='text/javascript'></script>
+<?php } ?>
 		<script type='text/javascript'>
 			// Define some global variables showing or hiding specific parts of the UI
 			// based on users' security rights
