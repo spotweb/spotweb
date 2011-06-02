fd538e50d2d7e3f08d232927357a43eb1942e238
nightspirit81 <ben@nightspirit.nl>
2011-06-02 13:04:03 +0200

    Fix #732

--- lib/SpotsOverview.php
+++ lib/SpotsOverview.php
@@ -473,7 +473,7 @@ class SpotsOverview {
 
 		# New spots
 		if (isset($search['filterValues']['New'])) {
-			if ($currentSession['prefs']['auto_markasread']) {
+			if ($currentSession['user']['prefs']['auto_markasread']) {
 				$newSpotsSearchTmp[] = '(s.stamp > ' . (int) $this->_db->safe( max($currentSession['user']['lastvisit'],$currentSession['user']['lastread']) ) . ')';
 			} else {
 				$newSpotsSearchTmp[] = '(s.stamp > ' . (int) $this->_db->safe($currentSession['user']['lastread']) . ')';
