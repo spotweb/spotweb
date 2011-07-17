5a7751da667e5395a905d88729dc338063195e91
rota <spotweb@hush.com>
2011-07-17 13:36:35 +0200

    Templates en ondersteunende JS toevoegen

---
+++ templates/we1rdo/listfilters.inc.php
@@ -0,0 +1,29 @@
+			<div class='filter'>
+				<ul id='sortablefilterlist' class='filterlist'>
+<?php			
+	function processFilters($tplHelper, $filterList) {
+		foreach($filterList as $filter) {
+			# Output de HTML
+			echo '<li class="sortable-element-class ' . $tplHelper->filter2cat($filter['tree']) . '" id="orderfilterslist_' . $filter['id'];
+			echo '"><div><a href="" onclick="return openDialog(\'editdialogdiv\', \'Bewerk een filter\', \'?page=render&tplname=editfilter&data[filterid]=' . $filter['id'] . '\', \'editfilterform\', true, function() { refreshTab(\'edituserpreferencetabs\')});">';
+			echo '<img src="images/icons/' . $filter['icon'] . '" alt="' . $filter['title'] . '">' . $filter['title'] . ' (' . $filter['id'] . ')' . '</a></div>';
+			
+			# Als er children zijn, output die ook
+			if (!empty($filter['children'])) {
+				echo '<ul>';
+				processFilters($tplHelper, $filter['children']);
+				echo '</ul>';
+			} # if
+			
+			echo '</li>' . PHP_EOL;
+		} # foreach
+	} # processFilters
+	
+	processFilters($tplHelper, $tplHelper->getUserFilterList());
+?>
+			</ul>
+	
+<script type='text/javascript'>	
+var editfilterformcsrfcookie = '<?php echo $tplHelper->generateXsrfCookie('editfilterform'); ?>';
+</script>
+			</div>
