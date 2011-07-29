e98b2fe25ae12e3ff77cc025fc9f34fd23e6e2d6
rota <spotweb@hush.com>
2011-07-29 19:52:44 +0200

    index_filter ook niet meer definieeren in settings.php

--- settings.php
+++ settings.php
@@ -163,12 +163,6 @@ $settings['enable_stacktrace'] = true;
 # dat je ooit hoeft in te loggen.
 $settings['nonauthenticated_userid'] = 1;
 
-# de filter die standaard gebruikt wordt op de index pagina (als er geen filters oid opgegeven zijn), 
-# zorg dat deze wel gedefinieerd is.
-$settings['index_filter'] = array();
-
-# als je standaard geen erotiek wilt op de index, uncomment dan volgende filter, je kan wel erotiek vinden door te zoeken
-# $settings['index_filter'] = array('tree' => '~cat0_z3');
 
 #
 # Hoeveel verschillende headers (van danwel spots danwel comments) moeten er per keer opgehaald worden? 
