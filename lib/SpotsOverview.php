b2e081dfb8b09419d8f58488197b6ef126fcd79e
rota <spotweb@hush.com>
2011-08-31 10:14:42 +0200

    Importeren vna filters hoort daar niet thuis

--- lib/SpotsOverview.php
+++ lib/SpotsOverview.php
@@ -172,42 +172,6 @@ class SpotsOverview {
 	} # loadSpots()
 
 	/*
-	 * Converteert een XML string naar een parsedSearch structure 
-	 */
-	public function xmlToParsedSearch($xmlStr, $currentSession) {
-		$search = array('tree' => '',
-						'value' => array());
-		$sortArray = array();
-		
-		# We creeeren een soort fake url zodat we de logica die al
-		# bestaat in het systeem simpel kunnen hergebruiken.
-		$xml = @(new SimpleXMLElement($xmlStr));
-		if (isset($xml->spotwebfilter->tree)) {
-			foreach($xml->spotwebfilter->tree->item as $item) {
-				#FIXME type attribute wordt genegeerd!
-				$search['tree'] .= $item . ',';
-			} # foreach
-		} # if
-		
-		if (isset($xml->spotwebfilter->filter)) {
-			foreach($xml->spotwebfilter->filter->item as $item) {
-				$search['value'][] = $item->fieldname . ':' . $item->operator . ':' . $item->value;
-			} # foreach
-		} # if
-		
-		if (isset($xml->spotwebfilter->sort)) {
-			foreach($xml->spotwebfilter->sort->item as $item) {
-				$sortArray = array('field' => (string) $item->fieldname, 'direction' => (string) $item->direction);
-			} # foreach
-		} # if
-
-		$parsedSearch = $this->filterToQuery($search, $sortArray, $currentSession, array());
-		return array('title' => $xml->title,
-					 'icon' => $xml->icon,
-					 'parsedSearch' => $parsedSearch);
-	} # xmlToParsedSearch 
-	
-	/*
 	 * Bereid een string met daarin categorieen voor en 'expand' 
 	 * die naar een complete string met alle subcategorieen daarin
 	 */
