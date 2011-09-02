d0b5d42e3919c01fa07722de0eda4dacfe9d7b3c
rota <spotweb@hush.com>
2011-09-03 00:15:04 +0200

    Template voor het uploaden en downloaden van filters

--- templates/we1rdo/listfilters.inc.php
+++ templates/we1rdo/listfilters.inc.php
@@ -30,17 +30,20 @@
 
 			<br>
 			
-			<form class="editfilterform" name="editfilterform" action="<?php echo $tplHelper->makeEditFilterAction(); ?>" method="post">
+			<form class="editfilterform" name="editfilterform" action="<?php echo $tplHelper->makeEditFilterAction(); ?>" method="post" enctype="multipart/form-data">
 				<input type="hidden" name="editfilterform[xsrfid]" value="<?php echo $tplHelper->generateXsrfCookie('editfilterform'); ?>">
 				<input type="hidden" name="filterid" value="9999">
 				<input class="greyButton" onclick="ajaxSubmitFormWithCb('?page=editfilter', this, function() { refreshTab('edituserpreferencetabs')}); return false;" type="submit" name="editfilterform[submitdiscardfilters]" value="Herstel filters naar standaard">
 <?php if ($tplHelper->allowed(SpotSecurity::spotsec_set_filters_as_default, ''))  { ?>
 				<input class="greyButton" onclick="ajaxSubmitFormWithCb('?page=editfilter', this, function() { refreshTab('edituserpreferencetabs')}); return false;" type="submit" name="editfilterform[submitsetfiltersasdefault]" value="Deze filters gebruiken als standaard">
 <?php } ?>
-<!--
 				<input class="greyButton" type="submit" name="editfilterform[submitexportfilters]" value="Download je filters">
--->
+
+				<input type="hidden" name="MAX_FILE_SIZE" value="30720" />
+				<input name="filterimport" type="file" />
+				<input class="greyButton" type="submit" name="editfilterform[submitimportfilters]" value="Upload je filters">
 			</form>
+			
 		</div>
 
 <script type='text/javascript'>	
