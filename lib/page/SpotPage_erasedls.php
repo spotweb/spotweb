543ada7c271adf7097f2d81fd5a97ba5a429b463
rota <spotweb@hush.com>
2011-09-03 11:19:25 +0200

    Wissen van downloads is nu een apart recht geworden (issue #935)

--- lib/page/SpotPage_erasedls.php
+++ lib/page/SpotPage_erasedls.php
@@ -4,6 +4,7 @@ class SpotPage_erasedls extends SpotPage_Abs {
 	function render() {
 		# Controleer de users' rechten
 		$this->_spotSec->fatalPermCheck(SpotSecurity::spotsec_keep_own_downloadlist, '');
+		$this->_spotSec->fatalPermCheck(SpotSecurity::spotsec_keep_own_downloadlist, 'erasedls');
 
 		$this->_tplHelper->clearDownloadList();
 		
