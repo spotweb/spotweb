8069ba87f78e79610d0781e0a2d45e6a2de2f790
rota <spotweb@hush.com>
2011-09-09 08:53:12 +0200

    Verdere kleine fixjes uit de static analysis van hiphop php

--- lib/exceptions/CustomException.php
+++ lib/exceptions/CustomException.php
@@ -24,7 +24,7 @@ abstract class CustomException extends Exception implements IException
     protected $code    = 0;                       // User-defined exception code
     protected $file;                              // Source filename of exception
     protected $line;                              // Source line of exception
-    private   $trace;                             // Unknown
+    //private   $trace;                             // Unknown
 
     public function __construct($message = null, $code = 0)
     {
