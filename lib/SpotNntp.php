b254388918801023be9abfeacdc99a0fd059ddc1
Carnifex <williamvdbosch@derptech.com>
2011-10-03 02:35:52 +0200

    minor bugfix

--- lib/SpotNntp.php
+++ lib/SpotNntp.php
@@ -562,8 +562,7 @@ class SpotNntp {
 			return $spot;
 		} # getFullSpot 
 		
-		function reportSpotAsSpam($user, $serverPrivKey, $title, $report) {
-			
+		function reportSpotAsSpam($user, $serverPrivKey, $title, $report) {			
 			# instantieer de benodigde objecten
 			$spotSigning = new SpotSigning();
 			$spotParser = new SpotParser();
@@ -572,13 +571,13 @@ class SpotNntp {
 			$user_signature = $spotSigning->signMessage($user['privatekey'], '<' . $report['newmessageid'] . '>');
 			
 			# ook door de php server 
-			$server_signature = $spotSigning->signMessage($serverPrivKey, $report['newmessageid']);
+			$server_signature = $spotSigning->signMessage($serverPrivKey, '<' . $report['newmessageid'] . '>');
 			
 			$header = 'From: ' . $user['username'] . " <" . trim($user['username']) . '@spot.net>' . "\r\n";
-			$header .= 'Subject: REPORT ' . $report['inreplyto'] . ' ' . $title . "\r\n";
+			$header .= 'Subject: REPORT <' . $report['inreplyto'] . '> ' . $title . "\r\n";
 			$header .= 'Newsgroups: free.willey' . "\r\n";
-			$header .= 'Message-ID: ' . $report['newmessageid'] . "\r\n";
-			$header .= 'References: ' . $report['inreplyto'] . "\r\n";
+			$header .= 'Message-ID: <' . $report['newmessageid'] . ">\r\n";
+			$header .= 'References: <' . $report['inreplyto'] . ">\r\n";
 			$header .= 'X-User-Signature: ' . $spotParser->specialString($user_signature['signature']) . "\r\n";
 			$header .= 'X-Server-Signature: ' . $spotParser->specialString($server_signature['signature']) . "\r\n";
 			$header .= 'X-User-Key: ' . $spotSigning->pubkeyToXml($user_signature['publickey']) . "\r\n";
