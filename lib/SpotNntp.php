018057d4c52b45bb543a042f82c1df44faa20760
rota <spotweb@hush.com>
2011-10-04 15:37:25 +0200

    Rating moet helemal weg

--- lib/SpotNntp.php
+++ lib/SpotNntp.php
@@ -462,7 +462,6 @@ class SpotNntp {
 			 */
 			$addHeaders = 'From: ' . $user['username'] . " <" . trim($user['username']) . '@spot.net>' . "\r\n";
 			$addHeaders .= 'References: <' . $report['inreplyto']. ">\r\n";
-			$addHeaders .= 'X-User-Rating: ' . (int) $report['rating'] . "\r\n";
 
 			return $this->postSignedMessage($user, $serverPrivKey, $newsgroup, $report, $addHeaders);
 		} # reportSpotAsSpam
