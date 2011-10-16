5ddd372b5b23d315f1ad1bfb4c5003c13b705e79
rota <spotweb@hush.com>
2011-10-16 16:14:54 +0200

    Editten van filters en reordenen van filters nu ook van de sprite gebruik laten maken

--- templates/we1rdo/listfilters.inc.php
+++ templates/we1rdo/listfilters.inc.php
@@ -10,7 +10,7 @@
 			# Output de HTML
 			echo '<li class="sortable-element-class ' . $tplHelper->filter2cat($filter['tree']) . '" id="orderfilterslist_' . $filter['id'];
 			echo '"><div><a href="" onclick="return openDialog(\'editdialogdiv\', \'Bewerk een filter\', \'?page=render&tplname=editfilter&data[filterid]=' . $filter['id'] . '\', \'editfilterform\', null, true, function() { refreshTab(\'edituserpreferencetabs\')});">';
-			echo '<img src="images/icons/' . $filter['icon'] . '" alt="' . $filter['title'] . '">' . $filter['title'] . '</a>';
+			echo '<span class="spoticon spoticon-' . str_replace('.png', '', $filter['icon']) . '">&nbsp;</span>' . $filter['title'] . '</a>';
 			echo '</div>';
 			
 			# Als er children zijn, output die ook
