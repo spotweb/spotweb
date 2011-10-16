6366ca731fb4894582cdf4eab9f7b913d0dd5a13
rota <spotweb@hush.com>
2011-10-16 22:38:33 +0200

    Nog een fix voor entities

--- lib/SpotTemplateHelper.php
+++ lib/SpotTemplateHelper.php
@@ -469,7 +469,7 @@ class SpotTemplateHelper {
 		# escape alle embedded HTML, maar eerst zetten we de spot inhoud om naar 
 		# volledige HTML, dit doen we omdat er soms embedded entities (&#237; e.d.) 
 		# in zitten welke we wel willen behouden.
-		$tmp = htmlentities($tmp);
+		$tmp = htmlentities($tmp, ENT_QUOTES, 'UTF-8');
 		$tmp = html_entity_decode($tmp, ENT_COMPAT, 'UTF-8');
 		
 		# Code gecopieerd vanaf 
