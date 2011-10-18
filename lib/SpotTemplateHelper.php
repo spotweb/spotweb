4ba120f0e33d91ea4b3ef2436814f5741a24c8aa
rota <spotweb@hush.com>
2011-10-18 07:23:55 +0200

    UTF8 in titels wordt nu goed meegenomen als spotsfull opgehaald wordt en anders enkel als de spot geopend wordt. Verder werd de tag perongeluk aan de titel vast geplakt in sommige gevallen bij het posten. #1036

--- lib/SpotTemplateHelper.php
+++ lib/SpotTemplateHelper.php
@@ -642,6 +642,8 @@ class SpotTemplateHelper {
 		$spot['posterurl'] = $this->makePosterUrl($spot);
 
 		// title escapen
+		$spot['title'] = htmlentities($spot['title'], ENT_QUOTES, 'UTF-8');
+		$spot['title'] = html_entity_decode($spot['title'], ENT_COMPAT, 'UTF-8');
 		$spot['title'] = htmlspecialchars(strip_tags($this->remove_extensive_dots($spot['title'])), ENT_QUOTES, 'UTF-8');
 		$spot['poster'] = htmlspecialchars(strip_tags($spot['poster']), ENT_QUOTES, 'UTF-8');
 		
