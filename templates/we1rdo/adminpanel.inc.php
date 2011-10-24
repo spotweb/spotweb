bedd29c213b1b6d927aa701942eae846e1858445
rota <spotweb@hush.com>
2011-10-24 12:59:12 +0200

    'Instellingen' tab verbergen in het admin panel, die is toch leeg. Fixes #1075

--- templates/we1rdo/adminpanel.inc.php
+++ templates/we1rdo/adminpanel.inc.php
@@ -6,7 +6,9 @@
 	<h4><a href='<?php echo $tplHelper->makeBaseUrl('path');?>'>&lt;&lt;&lt; Terug naar overzichtspagina</a></h4>
 	<div id="adminpaneltabs" class="ui-tabs">
 		<ul>
+<!--
 			<li><a href="#adminpaneltab-1" title="Instellingen"><span>Instellingen</span></a></li>
+-->
 <?php if ($tplHelper->allowed(SpotSecurity::spotsec_edit_other_users, '')) { ?>
 			<li><a href="?page=render&tplname=listusers" title="Gebruikerslijst"><span>Gebruikerslijst</span></a></li>
 <?php } ?>
