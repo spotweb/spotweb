e8b45c34272426120636621bdb65823180d23a5c
rota <spotweb@hush.com>
2011-11-11 18:34:54 +0100

    Only serve an error image if we are not in the retriever

--- lib/SpotsOverview.php
+++ lib/SpotsOverview.php
@@ -225,7 +225,7 @@ class SpotsOverview {
 		} # if
 
 		# bij een error toch een image serveren
-		if ($this->_activeRetriever) {
+		if (!$this->_activeRetriever) {
 			if ($return_code && $return_code != 200 && $return_code != 304) {
 				$data = $this->_spotImage->createErrorImage($return_code);
 			} elseif (!$data) {
