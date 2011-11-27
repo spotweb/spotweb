bd905a3a4e635c12bfad55b3c8f23f642fdf266e
rota <spotweb@hush.com>
2011-11-27 10:32:35 +0100

    Retrieve the IP address for auditing frm one central place, that should make the IP address always consistent and slightly faster

--- lib/SpotAudit.php
+++ lib/SpotAudit.php
@@ -3,26 +3,17 @@ class SpotAudit {
 	private $_db;
 	private $_user;
 	private $_settings;
+	private $_ipaddr;
 
-	function __construct(SpotDb $db, SpotSettings $settings, array $user) {
+	function __construct(SpotDb $db, SpotSettings $settings, array $user, $ipaddr) {
 		$this->_db = $db;
 		$this->_user = $user;
 		$this->_settings = $settings;
+		$this->_ipaddr = $ipaddr;
 	} # ctor
 	
 	function audit($perm, $objectid, $allowed) {
-		foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key) {
-			if (array_key_exists($key, $_SERVER) === true) {
-				foreach (explode(',', $_SERVER[$key]) as $ip) {
-					if (filter_var($ip, FILTER_VALIDATE_IP) !== false) {
-						$remote_addr = $ip;
-					} # if
-				} # foreach
-			} # if
-		} # foreach
-		$remote_addr = (isset($remote_addr)) ? $remote_addr : "unknown";
-
-		$this->_db->addAuditEntry($this->_user['userid'], $perm, $objectid, $allowed, $remote_addr);
+		$this->_db->addAuditEntry($this->_user['userid'], $perm, $objectid, $allowed, $this->_ipaddr);
 	} # audit
 	
 } # class SpotAudit
