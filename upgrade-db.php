9a14c28b9f08390348fab95037871cb2ef3376d5
rota <spotweb@hush.com>
2011-12-18 11:57:54 +0100

    Improved commandline support for retrieve.php so multiple parameters can be given

--- upgrade-db.php
+++ upgrade-db.php
@@ -2,7 +2,10 @@
 error_reporting(E_ALL & ~8192 & ~E_USER_WARNING);	# 8192 == E_DEPRECATED maar PHP < 5.3 heeft die niet
 
 try {
-	/*
+	require_once "lib/SpotClassAutoload.php";
+	require_once "settings.php";
+
+		/*
 	 * If we are run from another directory, try to change the current
 	 * working directory to a directory the script is in
 	 */
@@ -10,14 +13,13 @@ try {
 		chdir(__DIR__);
 	} # if
 
-	require_once "lib/SpotClassAutoload.php";
-	require_once "settings.php";
 
 	/*
 	 * Make sure we are not run from the server, an db upgrade can take too much time and
 	 * will easily be aborted by either a database, apache or browser timeout
 	 */
-	if (isset($_SERVER['SERVER_PROTOCOL'])) {
+	SpotCommandline::initialize(array(), array());
+	if (!SpotCommandline::isCommandline()) {
 		die("upgrade-db.php can only be run from the console, it cannot be run from the web browser");
 	} # if
 
