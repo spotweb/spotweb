90a54bbb8b27913fb6a66fb2d6c355b3a8ad92c4
spotweb <spotweb@hushmail.com>
2012-01-02 22:07:12 +0100

    Don't return an XML result when the login page is not rendered with the full UI but from the permdenied page. #1354

--- templates/we1rdo/login.inc.php
+++ templates/we1rdo/login.inc.php
@@ -1,23 +1,27 @@
 <?php
 if (!empty($loginresult)) {
-	if ((!isset($data['performredirect'])) || ($loginresult['result'] != 'success')) {
-		include 'includes/form-xmlresult.inc.php';
+	if ((!isset($data['performredirect'])) || (($loginresult['result'] != 'success'))) {
+		if (isset($data['showplainmessages'])) {
+			include 'includes/form-xmlresult.inc.php';
 		
-		$this->sendContentTypeHeader('xml');
-		echo formResult2Xml($loginresult, $formmessages, $tplHelper);
+			$this->sendContentTypeHeader('xml');
+			echo formResult2Xml($loginresult, $formmessages, $tplHelper);
+		} # if
 	} else {
 		$tplHelper->redirect($loginform['http_referer']);
 	} # if
 } # if
 
-if (($currentSession['user']['userid'] == $settings->get('nonauthenticated_userid')) && (empty($loginresult))) {
+if (($currentSession['user']['userid'] == $settings->get('nonauthenticated_userid')) && (empty($loginresult)) || (isset($data['showplainmessages']))) {
 	include "includes/form-messages.inc.php"; 
-
 ?>
 <form class="loginform" name="loginform" action="<?php echo $tplHelper->makeLoginAction(); ?>" method="post">
 	<input type="hidden" name="loginform[xsrfid]" value="<?php echo $tplHelper->generateXsrfCookie('loginform'); ?>">
 	<?php if (isset($data['performredirect'])) {?>
 		<input type="hidden" name="data[performredirect]" value="<?php echo $data['performredirect']; ?>">
+	<?php } ?>
+	<?php if (isset($data['showplainmessages'])) {?>
+		<input type="hidden" name="data[showplainmessages]" value="<?php echo $data['showplainmessages']; ?>">
 	<?php } ?>
 	<fieldset>
 		<dl>
