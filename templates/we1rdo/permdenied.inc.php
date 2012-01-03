4fdf9ac2978e14a6908752636bddd97cb317e147
spotweb <spotweb@hushmail.com>
2012-01-03 11:25:03 +0100

    Permission denied is now a proper html file and logging in upon an access denied will always redirect you to the page you got the access denied from. #1365

--- templates/we1rdo/permdenied.inc.php
+++ templates/we1rdo/permdenied.inc.php
@@ -1,31 +1,6 @@
-<html>
-	<head>
-		<style type='text/css'>
-			div.permdenied {
-				border: 1px solid red;
-				background-color: #fef1fc;
-				color: #cd0a0a;
-
-				font-size: 0.9em;
-				
-				margin-top: 4px;
-				margin-left: auto;
-				margin-right: auto;
-				
-				border-top-left-radius: 4px;
-				border-top-right-radius: 4px;
-				border-bottom-right-radius: 4px;
-				border-bottom-left-radius: 4px;
-			} 
-			
-			div.permdenied p {
-				text-align: center;
-			} 
-		</style>
-	</head>
-
-	<body>
-		<div class='container'>
+<?php
+	require_once "includes/basic-html-header.inc.php";
+?>
 			<div class='permdenied'>
 				<p>
 					<?php echo sprintf(_('Toegang geweigerd voor [%s (%d::%s)]'), $tplHelper->permToString($exception->getPermId()), $exception->getPermId(), $exception->getObject()); ?> </strong>
@@ -38,13 +13,15 @@
 	<?php
 		/* Als de user nog niet ingelogged is, geven we hem - mits hij dat recht heeft - de mogelijkheid in te loggen */
 		if ($tplHelper->allowed(SpotSecurity::spotsec_perform_login, '') && ($currentSession['user']['userid'] == $settings->get('nonauthenticated_userid'))) {
-			# loginform verwacht deze twee variables door de renderer, dus die faken we
+			# loginform verwacht deze drie variables door de renderer, dus die faken we
 			$data['performredirect'] = true;
-			$data['showplainmessages'] = true;
+			$data['renderhtml'] = true;
+			$data['htmlheaderssent'] = true;
 			$loginform = array('username' => '', 'password' => '');
-			
+			$http_referer = $tplHelper->makeSelfUrl('');
+						
 			require "login.inc.php";
-		} # if
+		} else {
+			echo "</body></html>";
+		} # else
 	?>
-	</body>
-</html>
