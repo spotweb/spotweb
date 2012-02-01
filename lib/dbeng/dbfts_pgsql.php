479ffc4f7fd447f64df3467cca0895a71c277810
spotweb <spotweb@hushmail.com>
2012-02-01 12:41:23 +0100

    Actually add the FTS engine to the system

---
+++ lib/dbeng/dbfts_pgsql.php
@@ -0,0 +1,49 @@
+<?php
+
+class dbfts_pgsql extends dbfts_abs {
+	/*
+	 * Constructs a query part to match textfields. Abstracted so we can use
+	 * a database specific FTS engine if one is provided by the DBMS
+	 */
+	function createTextQuery($searchFields) {
+		SpotTiming::start(__FUNCTION__);
+
+		/*
+		 * Initialize some basic values which are used as return values to
+		 * make sure always return a valid set
+		 */
+		$filterValueSql = array();
+		$additionalFields = array();
+		$sortFields = array();
+
+		foreach($searchFields as $searchItem) {
+			$searchValue = trim($searchItem['value']);
+			$field = $searchItem['fieldname'];
+
+			/*
+			 * if we get multiple textsearches, we sort them per order
+			 * in the system
+			 */
+			$tmpSortCounter = count($additionalFields);
+			
+			# Prepare the to_tsvector and to_tsquery strings
+			$ts_vector = "to_tsvector('Dutch', " . $field . ")";
+			$ts_query = "plainto_tsquery('" . $this->_db->safe(strtolower($searchValue)) . "')";
+			
+			$filterValueSql[] = " " . $ts_vector . " @@ " . $ts_query;
+			$additionalFields[] = " ts_rank(" . $ts_vector . ", " . $ts_query . ") AS searchrelevancy" . $tmpSortCounter;
+			$sortFields[] = array('field' => 'searchrelevancy' . $tmpSortCounter,
+								  'direction' => 'DESC',
+								  'autoadded' => true,
+								  'friendlyname' => null);
+		} # foreach
+
+		SpotTiming::stop(__FUNCTION__, array($filterValueSql,$additionalFields,$sortFields));
+		
+		return array('filterValueSql' => $filterValueSql,
+					 'additionalTables' => array(),
+					 'additionalFields' => $additionalFields,
+					 'sortFields' => $sortFields);
+	} # createTextQuery()
+
+} # dbfts_abs
---
+++ lib/dbeng/dbfts_sqlite.php
@@ -0,0 +1,50 @@
+<?php
+
+class dbfts_pgsql extends dbfts_abs {
+	/*
+	 * Constructs a query part to match textfields. Abstracted so we can use
+	 * a database specific FTS engine if one is provided by the DBMS
+	 */
+	function createTextQuery($searchFields) {
+		SpotTiming::start(__FUNCTION__);
+
+		/*
+		 * Initialize some basic values which are used as return values to
+		 * make sure always return a valid set
+		 */
+		$filterValueSql = array('(idx_fts_spots.rowid = s.rowid)');
+		$additionalTables = array('idx_fts_spots');
+		$matchList = array();
+
+		/*
+		 * sqlite can only use one WHERE clause for all textstring matches,
+		 * if you exceed this it throws an unrelated error and refuses the query
+		 * so we have to collapse all textqueries into one query 
+		 */
+		foreach($searchFields as $searchItem) {
+			$searchValue = trim($searchItem['value']);
+			
+			/*
+			 * The caller usually provides an expiciet table.fieldname 
+			 * for the select, but sqlite doesn't recgnize this in its
+			 * MATCH statement so we remove it and hope there is no
+			 * ambiguity
+			 */
+			$tmpField = explode('.', $searchItem['fieldname']);
+			$field = $tmpField[1];
+			
+			$matchList[] = $field . ':' . $this->_db->safe($searchValue);
+		} # foreach
+		
+		# add one WHERE MATCH conditions with all conditions
+		$filterValueSql[] = " (idx_fts_spots MATCH '" . implode(' ', $matchList) . "') ";
+		
+		SpotTiming::stop(__FUNCTION__, array($filterValueSql,$additionalTables));
+
+		return array('filterValueSql' => $filterValueSql,
+					 'additionalTables' => $additionalTables,
+					 'additionalFields' => array(),
+					 'sortFields' => array());
+	} # createTextQuery()
+
+} # dbfts_abs
