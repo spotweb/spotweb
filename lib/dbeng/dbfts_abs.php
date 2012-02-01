91c2d57e09eaef3c95ddec2f244121db9669ec63
spotweb <spotweb@hushmail.com>
2012-02-01 12:41:23 +0100

    Actually add the FTS engine to the system

---
+++ lib/dbeng/dbfts_abs.php
@@ -0,0 +1,52 @@
+<?php
+
+abstract class dbfts_abs {
+	protected $_db = null;
+				
+	/*
+	 * constructor
+	 */
+	function __construct(dbeng_abs $dbCon) {
+		$this->_db = $dbCon;		
+	} // ctor
+
+	/*
+	 * Returns the correct FTS class for the given dbclass
+	 */
+	static function Factory(dbeng_abs $db) {
+		if ($db instanceof dbeng_pdo_pgsql) {
+			return new dbfts_pgsql($db);
+		} elseif ($db instanceof dbeng_pdo_mysql) {
+			return new dbfts_mysql($db);
+		} elseif ($db instanceof dbeng_mysql) {
+			return new dbfts_mysql($db);
+		} elseif ($db instanceof dbeng_pdo_sqlite) {
+			return new dbfts_sqlite($db);
+		} else {
+			throw new NotImplementedException("Unknown database engine for FTS ?");
+		} # else
+	} # factory
+
+	/*
+	 * Constructs a query part to match textfields. Abstracted so we can use
+	 * a database specific FTS engine if one is provided by the DBMS
+	 */
+	function createTextQuery($searchFields) {
+		# Initialize some basic variables so our return statements are simple
+		$filterValueSql = array();
+
+		foreach($searchFields as $searchItem) {
+			$searchValue = trim($searchItem['value']);
+			$field = $searchItem['fieldname'];
+			
+			$filterValueSql[] = " (" . $searchItem['fieldname'] . " LIKE '%"  . $this->safe($searchValue) . "%') ";
+		} # foreach
+
+		return array('filterValueSql' => $filterValueSql,
+					 'additionalTables' => array(),
+					 'additionalFields' => array(),
+					 'sortFields' => array());
+	} # createTextQuery
+
+
+} # dbfts_abs
