0f0be035055625effa2903e19bac664004a06a95
spotweb <spotweb@hushmail.com>
2012-02-14 19:16:20 +0100

    Add a column to show which kind of spotterid listing this is (blacklist/whitelist)

--- templates/we1rdo/editspotterblacklist.inc.php
+++ templates/we1rdo/editspotterblacklist.inc.php
@@ -8,6 +8,7 @@
 		<thead>
 			<tr class="ui-widget-header">
 				<th><?php echo _('Spotter ID'); ?></th> 
+				<th><?php echo _('Type'); ?></th>
 				<th><?php echo _('Origin'); ?></th>
 				<th><?php echo _('Remove spotterid'); ?></th>
 			</tr>
@@ -18,6 +19,7 @@
 	foreach($blacklist as $bannedspotter) {
 ?>
 				<td> <?php echo $bannedspotter['spotterid']; ?> </td>
+				<td> <?php if ($bannedspotter['idtype'] == 1) { echo _("Blacklisted"); } else { echo ("Whitelisted"); } ?> </td>
 				<td> <?php echo $bannedspotter['origin']; ?> </td>
 				<td><a href="" onclick="return openDialog('editblacklistdialogdiv', '<?php echo _('Remove spotter from blacklist') ?>', '?page=render&tplname=editspotterblacklistdelete&data[spotterid]=<?php echo $bannedspotter['spotterid']; ?>', 'blacklistspotterform', null, 'autoclose', function() { refreshTab('edituserpreferencetabs')}, null); "><span class="ui-icon ui-icon-circle-close"></span></a></td>
 			</tr>
