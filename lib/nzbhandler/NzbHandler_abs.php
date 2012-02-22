55eab8d5926128edc110d4f58c763001e98772e9
Tjebbe <Tjebbe@192.168.1.107>
2012-02-22 11:43:25 +0100

    added isAvailable() for generic check if handler is available

--- lib/nzbhandler/NzbHandler_abs.php
+++ lib/nzbhandler/NzbHandler_abs.php
@@ -6,7 +6,7 @@ abstract class NzbHandler_abs
 
 	protected $_nzbHandling = null;
 	protected $_settings = null;
-	
+
 	function __construct($settings, $name, $nameShort, array $nzbHandling)
 	{
 		$this->_settings = $settings;
@@ -14,7 +14,7 @@ abstract class NzbHandler_abs
 		$this->_name = $name;
 		$this->_nameShort = $nameShort;
 	} # __construct
-	
+
 	/**
 	 * Get the name of the application handling the nzb, e.g. "SabNZBd".
 	 */
@@ -26,7 +26,7 @@ abstract class NzbHandler_abs
 	/**
 	 * Set the name of the application handling the nzb. This allows template
 	 * designers to adapt the application name if necessary
-	 */	
+	 */
 	public function setName($name)
 	{
 		$this->_name = $name;
@@ -34,7 +34,7 @@ abstract class NzbHandler_abs
 
 	/**
 	 * Get the name of the application handling the nzb, e.g. "SAB".
-	 */	
+	 */
 	public function getNameShort()
 	{
 		return $this->_nameShort;
@@ -44,12 +44,12 @@ abstract class NzbHandler_abs
 	 * Set the short name of the application handling the nzb. This allows template
 	 * designers to adapt the application name if necessary
 	 */
-	
+
 	public function setNameShort($name)
 	{
 		$this->_nameShort = $name;
 	} # setNameShort
-	
+
 	abstract public function processNzb($fullspot, $nzblist);
 
 	public function generateNzbHandlerUrl($spot, $spotwebApiParam)
@@ -57,10 +57,10 @@ abstract class NzbHandler_abs
 		$spotwebUrl = $this->_settings->get('spotweburl');
 		$action = $this->_nzbHandling['action'];
 		$url = $spotwebUrl . '?page=getnzb&amp;action=' . $action . '&amp;messageid=' . $spot['messageid'] . $spotwebApiParam;
-		
+
 		return $url;
 	} # generateNzbHandlerUrl
-	
+
 	/*
 	 * Genereert een schone filename voor nzb files
 	 */
@@ -68,34 +68,34 @@ abstract class NzbHandler_abs
 	{
 		$allowedChars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!,@#^()-=+ _{}';
 		$newTitle = '';
-		
+
 		for($i = 0; $i < strlen($title); $i++) {
 			if (stripos($allowedChars, $title[$i]) === false) {
 				$newTitle .= '_';
 			} else {
 				$newTitle .= $title[$i];
-			} 
+			}
 		} # for
-		
+
 		return $newTitle;
 	} # cleanForFileSystem
-	
-	/* 
+
+	/*
 	 * Genereert het volledige path naar de NZB locatie waar files opgeslagen moeten worden
 	 */
 	protected function makeNzbLocalPath($fullspot, $path)
 	{
 		$category = $this->convertCatToSabnzbdCat($fullspot);
-		
+
 		# add category to path als dat gevraagd is
 		$path = str_replace('$SABNZBDCAT', $this->cleanForFileSystem($category), $path);
 
 		# als de path niet eindigt met een backslash of forwardslash, voeg die zelf toe
 		$path = $this->addTrailingSlash($path);
-		
+
 		return $path;
 	} # makeNzbLocalPath
-	
+
 	/*
 	 * Voegt, indien nodig, een trailing slash toe
 	 */
@@ -105,10 +105,10 @@ abstract class NzbHandler_abs
 		if (strpos('\/', $path[strlen($path) - 1]) === false) {
 			$path .= DIRECTORY_SEPARATOR;
 		} # if
-		
+
 		return $path;
 	} # addTrailingSlash
-	
+
 	protected function sendHttpRequest($method, $url, $header, $content, $timeout = 15, $userAgent = 'Spotweb')
 	{
 		$stream_options = array('http' =>
@@ -122,23 +122,23 @@ abstract class NzbHandler_abs
 
 		return @file_get_contents($url, false, $ctx);
 	} # sendHttpRequest
-	
+
 	protected function prepareNzb($fullspot, $nzblist)
 	{
-		# nu we alle nzb files hebben, trekken we de 'file' secties eruit, 
+		# nu we alle nzb files hebben, trekken we de 'file' secties eruit,
 		# en plakken die in onze overkoepelende nzb
 		$result = array();
 		switch($this->_nzbHandling['prepare_action'])
 		{
 			case 'zip'	: {
-				$result['nzb'] = $this->zipNzbList($nzblist); 
+				$result['nzb'] = $this->zipNzbList($nzblist);
 				$result['mimetype'] = 'application/x-zip-compressed';
 				$result['filename'] = 'SpotWeb_' . microtime(true) . '.zip';
 				break;
 			} # zip
-			
+
 			default 		: {
-				$result['nzb'] = $this->mergeNzbList($nzblist); 
+				$result['nzb'] = $this->mergeNzbList($nzblist);
 				$result['mimetype'] = 'application/x-nzb';
 				$result['filename'] = $this->cleanForFileSystem($fullspot['title']) . '.nzb';
 				break;
@@ -147,17 +147,17 @@ abstract class NzbHandler_abs
 
 		return $result;
 	} # prepareNzb
-	
-	/* 
+
+	/*
 	 * Zet een Spot category om naar een sabnzbd category
 	 */
 	protected function convertCatToSabnzbdCat($spot) {
 		# fix de category
 		$spot['category'] = (int) $spot['category'];
-		
+
 		# vind een geschikte category
 		$sabnzbd = $this->_settings->get('sabnzbd');
-		
+
 		if (isset($sabnzbd['categories'][$spot['category']]['default'])) {
 			$category = $sabnzbd['categories'][$spot['category']]['default'];
 		} else {
@@ -166,10 +166,10 @@ abstract class NzbHandler_abs
 
 		foreach($spot['subcatlist'] as $cat) {
 			if (isset($sabnzbd['categories'][$spot['category']][$cat])) {
-				$category = $sabnzbd['categories'][$spot['category']][$cat];				
+				$category = $sabnzbd['categories'][$spot['category']][$cat];
 			} # if
 		} # foreach
-		
+
 		return $category;
 	} # convertCatToSabnzbdCat
 
@@ -184,7 +184,7 @@ abstract class NzbHandler_abs
 		$domNzbXml = dom_import_simplexml($nzbXml);
 		foreach($nzbList as $nzb) {
 			$oneNzbFile = simplexml_load_string($nzb['nzb']);
-			
+
 			# add each file section to the larger XML object
 			foreach($oneNzbFile->file as $file) {
 				# Import the file into the larger NZB object
@@ -205,39 +205,44 @@ abstract class NzbHandler_abs
 		if ($res !== TRUE) {
 			throw new Exception("Unable to create temporary ZIP file: " . $res);
 		} # if
-		
+
 		foreach($nzbList as $nzb) {
 			$zip->addFromString($this->cleanForFileSystem($nzb['spot']['title']) . '.nzb', $nzb['nzb']);
 		} # foreach
 		$zip->close();
-		
-		# lees de tempfile uit 
+
+		# lees de tempfile uit
 		$zipFile = file_get_contents($tmpZip);
-		
+
 		# en wis de tijdelijke file
 		unlink($tmpZip);
-		
+
 		return $zipFile;
-	} # zipNzbList	
+	} # zipNzbList
 
 	# NzbHandler API functions
 	public function hasApiSupport()
 	{
 		return false;
 	} # hasApiSupport
-	
+
 	public function getStatus()
 	{
 		# do nothing
 		return false;
 	} # getStatus
 
+        public function isAvailable()
+        {
+            return true;
+        } # isAvailable
+
 	public function pauseQueue()
 	{
 		# do nothing
 		return false;
 	} #pauseQueue
-	
+
 	public function resumeQueue()
 	{
 		# do nothing
@@ -255,13 +260,13 @@ abstract class NzbHandler_abs
 		# do nothing
 		return false;
 	} # moveDown
-	
+
 	public function moveUp($id)
 	{
 		# do nothing
 		return false;
 	} # moveUp
-	
+
 	public function moveTop($id)
 	{
 		# do nothing
@@ -273,13 +278,13 @@ abstract class NzbHandler_abs
 		# do nothing
 		return false;
 	} # moveBottom
-	
+
 	public function setCategory($id, $category)
 	{
 		# do nothing
 		return false;
 	} # setCategory
-	
+
 	public function setPriority($id, $priority)
 	{
 		# do nothing
@@ -290,26 +295,26 @@ abstract class NzbHandler_abs
 	{
 		# do nothing
 		return false;
-	} # setPassword	
-	
+	} # setPassword
+
 	public function delete($id)
 	{
 		# do nothing
 		return false;
 	} # delete
-	
+
 	public function rename($id, $name)
 	{
 		# do nothing
 		return false;
 	} # rename
-	
+
 	public function pause($id)
 	{
 		# do nothing
 		return false;
 	} # pause
-	
+
 	public function resume($id)
 	{
 		# do nothing
@@ -321,7 +326,7 @@ abstract class NzbHandler_abs
 		# For NzbHandlers that do not use configurable categories, but simply create
 		# category directories on demand (e.g. NZBGet) we'll just use the categories
 		# that are configured in SpotWeb.
-		
+
 		$sabnzbd = $this->_settings->get('sabnzbd');
 
 		$allcategories = array();
@@ -329,22 +334,22 @@ abstract class NzbHandler_abs
 		{
 			$allcategories = array_merge($allcategories, array_values($categories));
 		}
-		
+
 		$allcategories = array_unique($allcategories);
-		
+
 		$result = array();
 		$result['readonly'] = true;	// inform the GUI to not allow adding of adhoc categories
 		$result['categories'] = $allcategories;
-		
+
 		return $result;
 	} # getCategories
-	
-	
+
+
 	public function getVersion()
 	{
 		# do nothing
 		return false;
 	} # getVersion
-	
+
 } # class NzbHandler_abs
 
