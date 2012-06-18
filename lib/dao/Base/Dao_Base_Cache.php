0518d1b221f5198760e0dfb6fa99e4fa648a2a1c
spotweb <spotweb@hushmail.com>
2012-06-18 19:53:59 +0200

    PostgreSQL uses %b in storing the content, so we cannot share this code between PostgreSQL and sqlite

--- lib/dao/Base/Dao_Base_Cache.php
+++ lib/dao/Base/Dao_Base_Cache.php
@@ -45,34 +45,17 @@ class Dao_Base_Cache implements Dao_Cache {
 	} # getCache
 
 	/*
+	 * Add a resource to the cache
+	 */
+	function saveCache($resourceid, $cachetype, $metadata, $content) {
+		throw new NotImplementedException();
+	} # saveCache
+
+	/*
 	 * Refreshen the cache timestamp to prevent it from being stale
 	 */
 	function updateCacheStamp($resourceid, $cachetype) {
 		$this->_conn->exec("UPDATE cache SET stamp = %d WHERE resourceid = '%s' AND cachetype = '%s'", Array(time(), $resourceid, $cachetype));
 	} # updateCacheStamp
-
-	/*
-	 * Add a resource to the cache
-	 */
-	function saveCache($resourceid, $cachetype, $metadata, $content) {
-		if (is_array($content)) {
-			$serialize = true;
-			$content = serialize($content);
-		} else {
-			$serialize = false;
-		} # else
-
-		if ($metadata) {
-			$metadata = serialize($metadata);
-		} # if
-
-		$this->_conn->exec("UPDATE cache SET stamp = %d, metadata = '%s', serialized = '%s', content = '%s' WHERE resourceid = '%s' AND cachetype = '%s'", 
-									Array(time(), $metadata, $this->_conn->bool2dt($serialize), $content, $resourceid, $cachetype));
-		if ($this->_conn->rows() == 0) {
-			$this->_conn->modify("INSERT INTO cache(resourceid,cachetype,stamp,metadata,serialized,content) VALUES ('%s', '%s', %d, '%s', '%s', '%s')", 
-									Array($resourceid, $cachetype, time(), $metadata, $this->_conn->bool2dt($serialize), $content));
-		} # if
-	} # saveCache
-	
 
 } # Dao_Base_Cache
