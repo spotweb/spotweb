7878b63d7d9fd9d90238ec1ff12dd1eb2db27e96
spotweb <spotweb@hushmail.com>
2012-06-21 23:04:22 +0200

    Add Creation library, and move the legacy Spot parsing to its own class

--- lib/services/Format/Services_Format_Parsing.php
+++ lib/services/Format/Services_Format_Parsing.php
@@ -255,9 +255,12 @@ class Services_Format_Parsing {
 		} # if
 
 		if ((strpos($subj, '=?') !== false) && (strpos($subj, '?=') !== false)) {
+			# This is an old format to parse, instantiate the legacy parsing
+			$legacyParser = new Services_Format_ParsingLegacy();
+
 			# Make sure its as simple as possible
 			$subj = str_replace('?= =?', '?==?', $subj);
-			$subj = str_replace('\r', '', trim($this->oldEncodingParse($subj)));
+			$subj = str_replace('\r', '', trim($legacyParser->oldEncodingParse($subj)));
 			$subj = str_replace('\n', '', $subj);
 		} # if
 
@@ -285,7 +288,9 @@ class Services_Format_Parsing {
 			$spot['title'] = trim(implode('|', $tmp));
 
 			if ((strpos($spot['title'], chr(0xc2)) !== false) | (strpos($spot['title'], chr(0xc3)) !== false)) {
-				$spot['title'] = trim($this->oldEncodingParse($spot['title']));
+				# This is an old format to parse, instantiate the legacy parsing
+				$legacyParser = new Services_Format_ParsingLegacy();
+				$spot['title'] = trim($legacyParser->oldEncodingParse($spot['title']));
 			} # if
 		} # if recentKey
 
@@ -442,129 +447,6 @@ class Services_Format_Parsing {
 		return $strInput;
 	} # specialZipstr
 	
-	function parseEncodedWord($inputStr) {
-		$str = '';
-		$builder = '';
-
-		if (substr($inputStr, 0, 1) !== '=') {
-			return $inputStr;
-		} # if
-
-		if (substr($inputStr, strlen($inputStr) - 2) !== '?=') {
-			return $inputStr;
-		} # if
-
-		$name = substr($inputStr, 2, strpos($inputStr, '?', 2) - 2);
-		if (strtoupper($name) == 'UTF8') {
-			$name = 'UTF-8';
-		} # if
-
-		$c = $inputStr[strlen($name) + 3];
-		$startIndex = strlen($name) + 5;
-
-		switch(strtolower($c)) {
-			case 'q' :
-			{
-				while ($startIndex < strlen($inputStr)) {
-					$ch2 = $inputStr[$startIndex];
-					$chArray = null;
-
-					switch($ch2) {
-						case '=': {
-							if ($startIndex >= (strlen($inputStr) - 2)) {
-								$chArray = substr($inputStr, $startIndex + 1, 2);
-							} # if
-
-							if ($chArray == null) {
-								echo 'Untested code path!';
-								$builder .= $chArray . chr(10);
-								$startIndex += 3;
-							} # if 
-
-							continue;
-						} # case '=' 
-
-						case '?': {
-							if ($strInput[$startIndex + 1] == '=') {
-								$startIndex += 2;
-							} # if
-
-							continue;
-						} # case '?' 
-					} # switch
-
-					$builder .= $ch2;
-					$startIndex++;
-				} # while
-				break;
-			} # case 'q'
-
-			case 'b' :
-			{
-				$builder .= base64_decode(substr($inputStr, $startIndex, ((strlen($inputStr) - $startIndex) - 2)));
-				break;
-			} # case 'b'
-		} # switch
-
-		return $builder;
-	} # parseEncodedWord
-
-	function oldEncodingParse($inputStr) {
-		$builder = '';
-		$builder2 = '';
-		$encodedWord = false;
-		$num = 0;
-
-		while ($num < strlen($inputStr)) {
-			$bliep = false;
-			$ch = $inputStr[$num];
-
-			switch($ch) {
-				case '=' : 
-				{
-						if (($num != (strlen($inputStr) - 1)) && ($inputStr[$num + 1] == '?')) {
-							$encodedWord = true;
-						} # if
-						break;
-				} # case '='
-
-				case '?' :
-				{
-						$ch2 = ' ';
-
-						if ($num != (strlen($inputStr) - 1)) {
-							$ch2 = $inputStr[$num+1];
-						} # if
-
-						if ($ch2 != '=') {
-							break;
-						} # if
-
-						$encodedWord = false;
-						$builder .= $ch . $ch2;
-						$builder2 .= $this->parseEncodedWord($builder);
-						$builder = '';
-						$num += 2;
-						$bliep = true;
-						continue;						
-				} # case '?' 
-			} # switch
-
-			if (!$bliep) {
-				if ($encodedWord) {
-					$builder .= $ch;
-					$num++;
-				} else {	
-					$builder2 .= $ch;
-					$num++;
-				} # else
-			} # if
-
-		} # while
-
-		return $builder2;
-	} # oldEncodingParse
-
 	function specialString($strInput) {
 
 		$strInput = str_replace('/', '-s', $strInput);
---
+++ lib/services/Format/Services_Format_ParsingLegacy.php
@@ -0,0 +1,134 @@
+<?php
+
+
+/*
+ * Parser for the older and no longer used format (FTD spots)
+ */
+class Services_Format_ParsingLegacy {
+
+
+	private function parseEncodedWord($inputStr) {
+		$str = '';
+		$builder = '';
+
+		if (substr($inputStr, 0, 1) !== '=') {
+			return $inputStr;
+		} # if
+
+		if (substr($inputStr, strlen($inputStr) - 2) !== '?=') {
+			return $inputStr;
+		} # if
+
+		$name = substr($inputStr, 2, strpos($inputStr, '?', 2) - 2);
+		if (strtoupper($name) == 'UTF8') {
+			$name = 'UTF-8';
+		} # if
+
+		$c = $inputStr[strlen($name) + 3];
+		$startIndex = strlen($name) + 5;
+
+		switch(strtolower($c)) {
+			case 'q' :
+			{
+				while ($startIndex < strlen($inputStr)) {
+					$ch2 = $inputStr[$startIndex];
+					$chArray = null;
+
+					switch($ch2) {
+						case '=': {
+							if ($startIndex >= (strlen($inputStr) - 2)) {
+								$chArray = substr($inputStr, $startIndex + 1, 2);
+							} # if
+
+							if ($chArray == null) {
+								echo 'Untested code path!';
+								$builder .= $chArray . chr(10);
+								$startIndex += 3;
+							} # if 
+
+							continue;
+						} # case '=' 
+
+						case '?': {
+							if ($strInput[$startIndex + 1] == '=') {
+								$startIndex += 2;
+							} # if
+
+							continue;
+						} # case '?' 
+					} # switch
+
+					$builder .= $ch2;
+					$startIndex++;
+				} # while
+				break;
+			} # case 'q'
+
+			case 'b' :
+			{
+				$builder .= base64_decode(substr($inputStr, $startIndex, ((strlen($inputStr) - $startIndex) - 2)));
+				break;
+			} # case 'b'
+		} # switch
+
+		return $builder;
+	} # parseEncodedWord
+
+	public function oldEncodingParse($inputStr) {
+		$builder = '';
+		$builder2 = '';
+		$encodedWord = false;
+		$num = 0;
+
+		while ($num < strlen($inputStr)) {
+			$bliep = false;
+			$ch = $inputStr[$num];
+
+			switch($ch) {
+				case '=' : 
+				{
+						if (($num != (strlen($inputStr) - 1)) && ($inputStr[$num + 1] == '?')) {
+							$encodedWord = true;
+						} # if
+						break;
+				} # case '='
+
+				case '?' :
+				{
+						$ch2 = ' ';
+
+						if ($num != (strlen($inputStr) - 1)) {
+							$ch2 = $inputStr[$num+1];
+						} # if
+
+						if ($ch2 != '=') {
+							break;
+						} # if
+
+						$encodedWord = false;
+						$builder .= $ch . $ch2;
+						$builder2 .= $this->parseEncodedWord($builder);
+						$builder = '';
+						$num += 2;
+						$bliep = true;
+						continue;						
+				} # case '?' 
+			} # switch
+
+			if (!$bliep) {
+				if ($encodedWord) {
+					$builder .= $ch;
+					$num++;
+				} else {	
+					$builder2 .= $ch;
+					$num++;
+				} # else
+			} # if
+
+		} # while
+
+		return $builder2;
+	} # oldEncodingParse
+
+	
+} # Services_Format_ParsingLegacy
