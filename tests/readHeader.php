d3dca61ae0df7346b9a28fafdc1aef1914e5c0ca
spotweb <spotweb@example.com>
2012-09-16 12:32:58 +0200

    Updated tests for #1577

--- tests/readHeader.php
+++ tests/readHeader.php
@@ -1,15 +1,27 @@
 <?php
+require_once "lib/SpotClassAutoload.php";
+require_once "lib/SpotParser.php";
+require_once "lib/SpotPosting.php";
+require_once "lib/SpotCategories.php";
+require_once "lib/SpotNntp.php";
+require_once "NNTP/Protocol/Client.php";
+require_once "NNTP/Client.php";
+require_once "lib/exceptions/CustomException.php";
+require_once "lib/exceptions/NntpException.php";
+require_once "lib/services/Signing/Services_Signing_Base.php";
+require_once "lib/SpotSecurity.php";
+require_once "lib/SpotTiming.php";
+require_once "settings.php";
+require_once "lib/SpotDb.php";
 
-require "lib/SpotNntp.php";
-require "lib/SpotParser.php";
-require "NNTP/Protocol/Responsecode.php";
-require "NNTP/Protocol/Client.php";
-require "NNTP/Client.php";
-require "lib/services/Signing/Services_Signing_Base.php";
-require "lib/services/Signing/Services_Signing_Php.php";
-require "lib/services/Signing/Services_Signing_Openssl.php";
-require "lib/exceptions/CustomException.php";
-require "lib/exceptions/NntpException.php";
+
+
+$db = new SpotDb($settings['db']);
+$db->connect();
+
+echo "Connected to the database.." . PHP_EOL;
+
+$spotSettings = SpotSettings::singleton($db, $settings);
 
 $server = array('host' => $argv[1],
 				'enc' => false,
@@ -17,12 +29,30 @@ $server = array('host' => $argv[1],
 				'user' => $argv[2],
 				'pass' => $argv[3]);
 
+$rsaKeys = $spotSettings->get('rsa_keys');
+
 $nntp = new SpotNntp($server);
 $nntp->selectGroup('alt.test');
 try {
 	$r = $nntp->getHeader('<ZTZLM3pnNW5pE5RsDlWL8347ntp@spot.net>');
+       
+	foreach($r as $hdr) {
+		$y = explode(":", $hdr);
+		$r[$y[0]] = $y[1];
+	} # foreac
+ 
+	$spotParser = new SpotParser();
+        $spot = $spotParser->parseXover($r['Subject'],
+                                        $r['From'],
+                                        $r['Date'],
+                                        $r['Message-ID'],
+                                        $rsaKeys);
+
+	var_dump($y);
+	var_dump($r);
+        var_dump($spot);
+
 } catch(Exception $x) {
 		var_dump($x);
 }
-var_dump($r);
 
