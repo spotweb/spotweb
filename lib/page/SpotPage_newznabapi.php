c455be80a104fe4650971f53bdc3a2e1f36d45d0
spotweb <spotweb@hushmail.com>
2012-12-30 10:42:39 +0100

    Add parenthesis for the year

--- lib/page/SpotPage_newznabapi.php
+++ lib/page/SpotPage_newznabapi.php
@@ -139,11 +139,11 @@ class SpotPage_newznabapi extends SpotPage_Abs {
 			preg_match('/<time itemprop="datePublished" datetime="([0-9]{4})/ms', $imdb['content'], $movieReleaseDate);
 
 			/* Search for the title */
-			$search['value'][] = "Title:=:+\"" . trim($movieTitle[1]) . "\" +" . $movieReleaseDate[1];
+			$search['value'][] = "Title:=:+\"" . trim($movieTitle[1]) . "\" +(" . $movieReleaseDate[1] . ")";
 
 			/* IMDB  sometimes returns the title translated, if so, pass the original title as well */
 			if ((!empty($originalTitle)) && ($originalTitle[1] != $movieTitle[1])) {
-				$search['value'][] = "Title:=:+\"" . trim($originalTitle[1]) . "\" +" . $movieReleaseDate[1];
+				$search['value'][] = "Title:=:+\"" . trim($originalTitle[1]) . "\" +(" . $movieReleaseDate[1] . ")";
 			} // if
 
 		} elseif (!empty($this->_params['q'])) {
