83f7ef2cf919d630d09deedc4f8dfabeeabca167
Michael Withagen <michaelw@xs4all.nl>
2013-02-27 00:09:54 +0100

    Added support for alturl.com redirecting to binsearch. :)

--- lib/SpotAlternateDownload.php
+++ lib/SpotAlternateDownload.php
@@ -26,6 +26,7 @@ class SpotAlternateDownload {
 	  // Array containing url matches. Must contain the first part of the url.
 	  $matches = array(
 	  	'http://base64.derefer.me',
+	    'http://alturl.com'
 	  );
 	  
 	  // Search in the website url
@@ -81,8 +82,9 @@ class SpotAlternateDownload {
     $chOptions = array(
       CURLOPT_RETURNTRANSFER => 1,
       CURLOPT_FAILONERROR		 => 1,
-      CURLOPT_CONNECTTIMEOUT => 10,
+      CURLOPT_CONNECTTIMEOUT => 30,
       CURLOPT_FOLLOWLOCATION => TRUE,
+      CURLOPT_MAXREDIRS      => 20,
       CURLOPT_HEADER         => TRUE,
       CURLOPT_NOBODY         => TRUE,
       CURLOPT_SSL_VERIFYPEER => FALSE,
@@ -104,6 +106,8 @@ class SpotAlternateDownload {
      if ($finalUrl) {
        return $this->resolveMetaRefreshOnUrl($finalUrl);
      }
+    } else {
+      trigger_error(curl_errno($ch) . ': ' . curl_error($ch));
     }
     
     // Close handle (will occur on error)
@@ -133,8 +137,9 @@ class SpotAlternateDownload {
     $chOptions = array(
       CURLOPT_RETURNTRANSFER => 1,
       CURLOPT_FAILONERROR		 => 1,
-      CURLOPT_CONNECTTIMEOUT => 10,
-      CURLOPT_NOBODY         => false,
+      CURLOPT_CONNECTTIMEOUT => 30,
+      CURLOPT_ENCODING       => '',
+      CURLOPT_NOBODY         => FALSE,
       CURLOPT_SSL_VERIFYPEER => FALSE,
       CURLOPT_SSL_VERIFYHOST => FALSE,         
     );
@@ -159,6 +164,8 @@ class SpotAlternateDownload {
       curl_close($ch);
        
       return $url;
+    } else {
+      trigger_error(curl_errno($ch) . ': ' . curl_error($ch));
     }
 
     // Close handle (will occur on error)
@@ -213,22 +220,23 @@ class SpotAlternateDownload {
 	  
 	  // Get the alternate url.
     $url = $this->getUrlForSpot();
-    
+            
     // If there is no alternate url return;
     if (!$url) {
       $this->nzb = false;
       return null;
     }
-    
+
     // Initialize curl.
     $ch = curl_init($url);
     $chOptions = array(
       CURLOPT_RETURNTRANSFER => 1,
       CURLOPT_FAILONERROR		 => 1,
-      CURLOPT_CONNECTTIMEOUT => 10,
-      CURLOPT_NOBODY         => false,
+      CURLOPT_CONNECTTIMEOUT => 30,
+      CURLOPT_ENCODING       => '',
+      CURLOPT_NOBODY         => FALSE,
       CURLOPT_SSL_VERIFYPEER => FALSE,
-      CURLOPT_SSL_VERIFYHOST => FALSE,         
+      CURLOPT_SSL_VERIFYHOST => FALSE,     
     );
     curl_setopt_array($ch, $chOptions);
     
@@ -246,9 +254,16 @@ class SpotAlternateDownload {
       if (simplexml_load_string($body)) {
         $this->nzb = $body;
         return $this->nzb;
+      } else if($body) {
+        // we did not get a direct link to an nzb file.
+        // more parsing is needed t(*_*t)
+        $this->nzb = $this->downloadNzbFrom($url, $body);
+        return $this->nzb;
       } else {
         $this->nzb = false;
       }
+    } else {
+      trigger_error(curl_errno($ch) . ': ' . curl_error($ch));
     }
 
     // Close handle (will occur on error)
@@ -256,6 +271,144 @@ class SpotAlternateDownload {
     
     // Return nothing we have a curl error.
     return null;
+	}
+	
+	/**
+	 * 
+	 * Cases for calling the specific parse methods
+	 * 
+	 * @param String $url
+	 * @param String $body
+	 */
+	protected function downloadNzbFrom($url, $body) {
+	  // Binsearch
+	  if (strpos($url, 'binsearch.info') !== FALSE) {
+	    return $this->downloadNzbFromBinsearch($url, $body);
+	  }
+	  
+	  // No support found return ;(
+	  return false;
+	}
+	
+	/**
+	 * 
+	 * Tries to download the actual nzb from binsearch
+	 * 
+	 * @param String $url
+	 * @param String $body
+	 */
+	protected function downloadNzbFromBinsearch($url, $body) {	  
+	  // Match to get the nzb id.
+	  preg_match('/\q\=([a-z0-9]*)&/i', $url, $matches);
+
+	  // This match is essential for the download
+	  if (!count($matches)) {
+	    return false;
+	  }
+
+	  // Hardcoded download url.
+	  $downloadUrl = 'http://www.binsearch.info/fcgi/nzb.fcgi?q=' . $matches[1];
+
+    $dom = new DOMDocument;
+    
+    // Suppress errors, html does not have to be well formed to function.
+    @$dom->loadHTML( $body );
+    $ids = array();
+    
+    // Fetch table rows from the result page.
+    foreach( $dom->getElementsByTagName( 'tr' ) as $tr ) {
+      
+      // Only continue parsing if the search query is found in the tr.
+      if (strpos($tr->nodeValue, 'G7pruRWDr2ASFraTyabR5VagE5') !== false) {
+        
+        // Get all input fields.
+        $fields = $tr->getElementsByTagName('input');
+    
+        // Check type, we need the checkbox :)
+        foreach($fields as $input) {
+          if($input->getAttribute('type') == 'checkbox') {
+            
+            // walk up the DOM tree and check if the next element has the string in the name.
+            // this way we only have the download rows left.
+            if(
+              $input->parentNode && 
+              $input->parentNode->nextSibling 
+              && strpos($input->parentNode->nextSibling->nodeValue, 'G7pruRWDr2ASFraTyabR5VagE5') !== false) 
+            {
+              
+              // Push name to array. This name is needed to fetch the download.
+              $ids[] = $input->getAttribute('name');
+            }
+          }
+          
+        }
+        
+      }
+    }
+    
+    // Fetch the last id assuming our download was the first to post.
+    // This step is to prevent accidental porn downloads.
+    $id = null;
+    if (count($ids)) {
+      $id = array_pop($ids);
+    }
+
+    // Withoud an id where not going to be able to get the nzb.
+    if(!$id){
+      return false;
+    }
+	  
+	  $postdata = array(
+	    'action' => 'nzb',
+	    $id      => $id
+	  );
+
+	  return $this->postAndDownloadNzb($downloadUrl, $postdata);
+	}
+	
+	/**
+	 * 
+	 * Execute a POST to the given url and return the body.
+	 * @param String $url
+	 * @param array $postdata
+	 */
+	protected function postAndDownloadNzb($url, Array $postdata) {
+	  
+	  $fields_string = null;
+	  foreach($postdata as $key=>$value) { 
+	    $fields_string .= $key.'='.urlencode($value).'&'; 
+	  }
+    rtrim($fields_string, '&');
+
+	  // Initialize curl.
+    $ch = curl_init($url);
+    $chOptions = array(
+      CURLOPT_RETURNTRANSFER => 1,
+      CURLOPT_FAILONERROR		 => 1,
+      CURLOPT_CONNECTTIMEOUT => 30,
+      CURLOPT_ENCODING       => '',
+      CURLOPT_NOBODY         => FALSE,
+      CURLOPT_SSL_VERIFYPEER => FALSE,
+      CURLOPT_SSL_VERIFYHOST => FALSE,
+      CURLOPT_POST           => TRUE,
+      CURLOPT_POSTFIELDS     => $fields_string
+    );
+    curl_setopt_array($ch, $chOptions);
+    
+    // Execute
+    $body = curl_exec($ch);
+    
+    // Check if any error occured
+    if(!curl_errno($ch))
+    {
+      // Close handle
+      curl_close($ch);
+      return $body;
+    } else {
+      trigger_error(curl_errno($ch) . ': ' . curl_error($ch));
+    }
+
+    return false;
 	}
 	
 }
