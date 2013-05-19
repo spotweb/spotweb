d80a7b46f8d3c08604ed170946f21b3a8a7febde
spotweb <spotweb@hushmail.com>
2013-05-19 12:39:43 +0200

    Make sure to provide the fully qualified path to the CNF file, this fixes some issues with OpenSSL and Windows

--- lib/services/Signing/Services_Signing_Openssl.php
+++ lib/services/Signing/Services_Signing_Openssl.php
@@ -50,8 +50,7 @@ class Services_Signing_Openssl extends Services_Signing_Base {
 		$rsa = new Crypt_RSA();
 		$rsa->setSignatureMode(CRYPT_RSA_SIGNATURE_PKCS1);
 			
-		$opensslPrivKey = openssl_pkey_new(array('private_key_bits' => 1024, 'config' => $sslCnfPath));
-		openssl_pkey_export($opensslPrivKey, $privateKey, null, array('config' => $sslCnfPath));
+		$opensslPrivKey = openssl_pkey_new(array('private_key_bits' => 1024, 'config' => realpath($sslCnfPath)));
 		$publicKey = openssl_pkey_get_details($opensslPrivKey);
 		$publicKey = $publicKey['key'];
 		openssl_free_key($opensslPrivKey);
