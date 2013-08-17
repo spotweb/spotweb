f21e9d9080df5497f9bec66ab8c4976045b00318
spotweb <spotweb@hushmail.com>
2013-08-17 11:03:04 +0200

    Incorrect check for an image or nzb file to be present in database yet

--- lib/services/Retriever/Services_Retriever_Spots.php
+++ lib/services/Retriever/Services_Retriever_Spots.php
@@ -445,7 +445,7 @@ class Services_Retriever_Spots extends Services_Retriever_Base {
 						if ($this->_prefetch_image) {
                             $this->debug('foreach-loop, getImage(), start. msgId= ' . $msgId);
 
-                            if (isset($cachedIdList[Dao_Cache::SpotImage][$fullSpot['messageid']])) {
+                            if (!isset($cachedIdList[Dao_Cache::SpotImage][$fullSpot['messageid']])) {
                                 $this->_svcProvImage->fetchSpotImage($fullSpot);
                             } # if
 
@@ -464,7 +464,7 @@ class Services_Retriever_Spots extends Services_Retriever_Base {
 							if (!empty($fullSpot['nzb']) && $fullSpot['stamp'] > 1290578400) {
 								$this->debug('foreach-loop, getNzb(), start. msgId= ' . $msgId);
 
-                                if (isset($cachedIdList[Dao_Cache::SpotNzb][$fullSpot['messageid']])) {
+                                if (!isset($cachedIdList[Dao_Cache::SpotNzb][$fullSpot['messageid']])) {
                                     $this->_svcProvNzb->fetchNzb($fullSpot);
                                 } # if
 								$this->debug('foreach-loop, getNzb(), done. msgId= ' . $msgId);
