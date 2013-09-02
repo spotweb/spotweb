604400eebfe2ed98f53b4d8531b932f00513c8fa
spotweb <spotweb@hushmail.com>
2013-09-02 19:09:39 +0200

    Postgresql's fts parser did an OR between the tsearch2 and ilike part which should have been an "AND"

--- lib/dbeng/dbfts_pgsql.php
+++ lib/dbeng/dbfts_pgsql.php
@@ -82,7 +82,7 @@ class dbfts_pgsql extends dbfts_abs {
 
                 if (!empty($o_parse->tsearch)) {
                     $ts_query = "to_tsquery('Dutch', '" . $this->_db->safe($o_parse->tsearch) . "')";
-                    $filterValueSql[] = " " . $ts_vector . " @@ " . $ts_query;
+                    $queryPart[] = " " . $ts_vector . " @@ " . $ts_query;
                     $additionalFields[] = " ts_rank(" . $ts_vector . ", " . $ts_query . ") AS searchrelevancy" . $tmpSortCounter;
                 } # if
 
