07f9f76d274263d084d33d9d78241adbd7ebea0a
spotweb <spotweb@hushmail.com>
2014-01-02 10:40:09 +0100

    Force collation on MySQL connection to be utf8_unicode_ci so we can make sure our diacritics are not removed.

--- lib/dbeng/dbeng_pdo_mysql.php
+++ lib/dbeng/dbeng_pdo_mysql.php
@@ -22,15 +22,24 @@ class dbeng_pdo_mysql extends dbeng_pdo {
 			}
 
 			try {
+                /*
+                 * We specify the charset in the connection string to avoid a possible
+                 * sqli by using charset encoding. Hoewever, early versions of PHP
+                 * (<5.3.6) do not implement this properly, so we also set it using
+                 * the init string.
+                 *
+                 * We need to set the collate string, to avoid MySQL from removing
+                 * diacritics
+                 */
 				$this->_conn = new PDO('mysql:' . $db_conn . ';dbname=' . $db . ';charset=utf8',
                                         $user,
                                         $pass,
-                                        array(PDO::MYSQL_ATTR_FOUND_ROWS => true));
+                                        array(PDO::MYSQL_ATTR_FOUND_ROWS => true,
+                                              PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                                              PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8 COLLATE utf8_unicode_ci'));
 			} catch (PDOException $e) {
 				throw new DatabaseConnectionException($e->getMessage(), -1);
 			}
-
-			$this->_conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		} # if
 	} # connect()
 
