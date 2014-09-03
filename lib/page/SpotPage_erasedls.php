d5de54c4aac14007b2bc70c8f86801a5dea4810b
dmzx <info@dmzx-web.net>
2014-09-03 11:28:10 +0200

    Update SpotPage_erasedls.php

--- lib/page/SpotPage_erasedls.php
+++ lib/page/SpotPage_erasedls.php
@@ -9,6 +9,7 @@ class SpotPage_erasedls extends SpotPage_Abs {
 
 		# Instantiat the user system as necessary for the management of user preferences
 		$svcUserRecord = new Services_User_record($this->_daoFactory, $this->_settings);
+		$svcUserRecord = new Services_User_Record($this->_daoFactory, $this->_settings);
 		$svcUserRecord->clearDownloadList($this->_currentSession['user']['userid']);
 		
 		$this->sendExpireHeaders(true);
