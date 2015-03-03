969c484c6a421d7f1a55714351607fcab58bd64d
Sweepr <sphaera@upcmail.nl>
2015-03-03 18:41:11 +0100

    Update newspots_for_filter.inc.php
    Language changed into English

--- templates/notifications/newspots_for_filter.inc.php
+++ templates/notifications/newspots_for_filter.inc.php
@@ -1,3 +1,3 @@
-<?php echo $newCount; ?> nieuwe spots beschikbaar in filter "<?php echo $filtertitle; ?>"
+<?php echo $newCount; ?> new spots available for filter "<?php echo $filtertitle; ?>"
 
-<?php echo $filtertitle; ?> heeft <?php echo $newCount; ?> spots beschikbaar
+<?php echo $filtertitle; ?> has <?php echo $newCount; ?> spots available
