5bb13c14787fbd7f55fdea5288a6efc55c961cd2
Sweepr <sphaera@upcmail.nl>
2015-03-03 16:35:31 +0100

    Update spotinfo.inc.php

--- templates/mobile/spotinfo.inc.php
+++ templates/mobile/spotinfo.inc.php
@@ -15,7 +15,7 @@
 <?php if (!empty($spot['sabnzbdurl'])) { ?>
                   <a href="<?php echo str_replace('getnzb','getnzbmobile',$spot['sabnzbdurl']);?>"  data-transition='fade' data-rel="dialog" data-icon="plus" class="ui-btn-right">SAVE</a></th>
 <?php } else { ?>	
-  <a class="nzb" href="<?php echo $setpath ?>index.php?page=getnzbmobile&amp;messageid=<?php echo $spot['messageid']; ?>" target="_blank" title='' data-transition='fade' data-icon="plus" data-rel="dialog" class="ui-btn-right">NZB</a>
+  <a class="nzb" href="<?php echo $setpath ?>index.php?page=getnzb&amp;messageid=<?php echo $spot['messageid']; ?>" target="_blank" title='' data-transition='fade' data-icon="plus" data-rel="dialog" class="ui-btn-right">NZB</a>
 <?php } ?>		
 	
 	</div>
