887c1d46271e53d95efc5a5136605da8a6c23f9e
Sweepr <sphaera@upcmail.nl>
2015-03-03 18:33:36 +0100

    Update user_added_email.inc.php
    Language changed to English

--- templates/notifications/user_added_email.inc.php
+++ templates/notifications/user_added_email.inc.php
@@ -1,13 +1,13 @@
-Spotweb registratie
+Spotweb registration
 
-Hallo <?php echo $user['firstname'] . ' ' . $user['lastname'] ?>,
+Hello <?php echo $user['firstname'] . ' ' . $user['lastname'] ?>,
 
-Er is zojuist een account voor je aangemaakt op <?php echo $settings->get('spotweburl'); ?>.
+An account hads been created for you at <?php echo $settings->get('spotweburl'); ?>.
 
-Je kunt inloggen met de volgende gegevens:
+You can login using the following credentials:
 
-Gebruikersnaam:		<?php echo $user['username']; ?> 
-Wachtwoord:		<?php echo $user['newpassword1']; ?> 
+Username:		<?php echo $user['username']; ?> 
+Password:		<?php echo $user['newpassword1']; ?> 
 
-Met vriendelijke groet,
+Kind regards,
 <?php echo $adminUser['firstname'] . ' ' . $adminUser['lastname']; ?>.
