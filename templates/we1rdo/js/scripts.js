7bfb69c186169de97060fbd8e6a07a15d450f1ff
Jan <mesa57@hotmail.com>
2016-01-31 11:14:51 +0100

    Undo Merge pull request #92 from gijsroge/master (If screen bigger than content load in other results.). Broke settings screens.
    Conflicts:
    	templates/we1rdo/js/scripts.js

--- templates/we1rdo/js/scripts.js
+++ templates/we1rdo/js/scripts.js
@@ -165,49 +165,31 @@ function attachInfiniteScroll() {
 	//ready
 // console.time("2nd-ready");
 	var pagenr = $('#nextPage').val();
-
-	/**
-	 * Monitor scroll once bottom page has been reached load extra products
-	 */
 	$(window).scroll(function() {
 		var url = '?direction=next&data[spotsonly]=1&pagenr='+pagenr+$('#getURL').val()+' #spots';
+
 		if($(document).scrollTop() >= $(document).height() - $(window).height() && $(document).height() >= $(window).height() && pagenr > 0 && $("#overlay").is(':hidden')) {
-			loadProducts(url);
+			if(!($("div.spots").hasClass("full"))) {
+				var scrollLocation = $("div.container").scrollTop();
+				$("#overlay").show().addClass('loading');
+				$("div#overlay").load(url, function() {				
+					if($("div#overlay tbody#spots").children().size() < $('#perPage').val()) {
+						$("table.footer").remove();
+						$("div.spots").addClass("full");
+					}
+					$("#overlay").hide().removeClass('loading'); 
+					$("tbody#spots").append($($("div#overlay tbody#spots").html()).show());
+					$("div#overlay").empty();
+					$("a.spotlink").click(function(e) { e.preventDefault(); });
+					$(".showTipTip a.spotlink").each(applyTipTip);
+					
+					pagenr++;
+					$("td.next > a").attr("href", url);
+					$("div.container").scrollTop(scrollLocation);
+				});
+			}
 		}
 	});
-
-	/**
-	 * If user has big screen load extra products
-	 */
-	if($('.spots').outerHeight() < $(window).height()){
-		var url = '?direction=next&data[spotsonly]=1&pagenr='+pagenr+$('#getURL').val()+' #spots';
-		loadProducts(url);
-	}
-
-	/**
-	 * Load products
-	 */
-	var loadProducts = function(url){
-		if(!($("div.spots").hasClass("full"))) {
-			var scrollLocation = $("div.container").scrollTop();
-			$("#overlay").show().addClass('loading');
-			$("div#overlay").load(url, function() {
-				if($("div#overlay tbody#spots").children().size() < $('#perPage').val()) {
-					$("table.footer").remove();
-					$("div.spots").addClass("full");
-				}
-				$("#overlay").hide().removeClass('loading');
-				$("tbody#spots").append($($("div#overlay tbody#spots").html()).show());
-				$("div#overlay").empty();
-				$("a.spotlink").click(function(e) { e.preventDefault(); });
-				$(".showTipTip a.spotlink").each(applyTipTip);
-
-				pagenr++;
-				$("td.next > a").attr("href", url);
-				$("div.container").scrollTop(scrollLocation);
-			});
-		}
-	}
 // console.timeEnd("2nd-ready");
 } // attachInfiniteScroll
 
