91da26c9a684ef5bfe1eec93b2e8804d1bcf5716
mesa57 <mesa57@hotmail.com>
2016-05-20 13:05:50 +0200

    Revert "Issue #162 : corrections for sqlite"
    This reverts commit d1dffe2e105ed10d565cd14d385cfde2d2f9d118.

--- lib/dbeng/dbfts_sqlite.php
+++ lib/dbeng/dbfts_sqlite.php
@@ -7,24 +7,13 @@ class dbfts_sqlite extends dbfts_abs {
 	 */
 	function createTextQuery($searchFields, $additionalFields) {
 		SpotTiming::start(__CLASS__ . '::' . __FUNCTION__);
-        //var_dump($searchFields);
-        /*
+
+		/*
 		 * Initialize some basic values which are used as return values to
 		 * make sure always return a valid set
 		 */
-        $fieldIndex = array('title' => 1, 'poster' => 2, 'tag' => 3);
-        
-        // Determine additional table to use
-        $tmpField = explode('.', $searchFields[0]['fieldname']);
-        $columnField = strtolower($tmpField[1]);
-
-        $idxnum = $fieldIndex[$columnField];
-        if (empty($idxnum)) {
-            throw new NotImplementedException("Undefined SQLite FTS column: " . $columnField);
-        }
-                
-        $additionalTables = array(' idx_fts_spots AS idx_fts_spots_' . $idxnum);
-        $filterValueSql = array();      
+		$filterValueSql = array('(idx_fts_spots.rowid = s.rowid)');
+		$additionalTables = array('idx_fts_spots');
 		$matchList = array();
 
 		/*
@@ -32,9 +21,9 @@ class dbfts_sqlite extends dbfts_abs {
 		 * if you exceed this it throws an unrelated error and refuses the query
 		 * so we have to collapse all textqueries into one query 
 		 */
-
 		foreach($searchFields as $searchItem) {
 			$searchValue = trim($searchItem['value']);
+			
 			/*
 			 * The caller usually provides an expiciet table.fieldname 
 			 * for the select, but sqlite doesn't recgnize this in its
@@ -42,12 +31,13 @@ class dbfts_sqlite extends dbfts_abs {
 			 * ambiguity
 			 */
 			$tmpField = explode('.', $searchItem['fieldname']);
-			$matchList[] = $searchValue;
+			$field = $tmpField[1];
+			
+			$matchList[] = $field . ':' . $this->_db->safe($searchValue);
 		} # foreach
 		
 		# add one WHERE MATCH conditions with all conditions
-
-        $filterValueSql[] =  '(idx_fts_spots_'. $idxnum .'.docid = s.rowid) AND ' . " (idx_fts_spots_" . $idxnum . ".". $columnField . " MATCH '" . implode(' ', $matchList) . "') ";
+		$filterValueSql[] = " (idx_fts_spots MATCH '" . implode(' ', $matchList) . "') ";
 		
 		SpotTiming::stop(__CLASS__ . '::' . __FUNCTION__, array($filterValueSql,$additionalTables));
 
