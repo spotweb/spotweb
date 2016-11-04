spotweb/spotweb

commit 022d7ac8e6f78fb6e6796abaad227a29817034bb

Reverted retriever.php

retrieve.php
@@ -9,7 +9,7 @@ try {
 	 */
     chdir(__DIR__ . '/../');
 
-	require_once __DIR__ . '/../vendor/autoload.php';
+	require_once __DIR__ . '/vendor/autoload.php';
 
 	/*
 	 * Initialize the Spotweb base classes

commit 165b0c39cef04a36595af4d2b00d14961811d6b6

Don't change to parent directory
Fix for bug introduced in commit 022d7ac which makes dbsettings.inc.php
unfindable

retrieve.php
@@ -3,12 +3,6 @@
 error_reporting(2147483647);
 
 try {
-	/*
-	 * If we are run from another directory, try to change the current
-	 * working directory to a directory the script is in
-	 */
-    chdir(__DIR__ . '/../');
-
 	require_once __DIR__ . '/vendor/autoload.php';
 
 	/*