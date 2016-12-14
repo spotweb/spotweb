bf8d6995e69ec902475e207707d201c7069da8a8
Get your own 'tots. Geez! <YipYup@users.noreply.github.com>
2016-12-14 07:01:45 -0700

    Update fatalerror.inc.php

--- templates/installer/fatalerror.inc.php
+++ templates/installer/fatalerror.inc.php
@@ -1,4 +1,4 @@
 <div id='error'><?php echo $x->getMessage(); ?>
     <br/><br/>
-    Please correct the errors in below form and try again
+    Please correct the errors in below form and try again.
 </div>
