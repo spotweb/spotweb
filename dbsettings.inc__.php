e9669f61860097020d924681ca8893ddf7c9eb33
root <root@sp1r1t.xxx-image.com>
2020-07-26 10:59:29 +0200

    Fix for issue: https://github.com/spotweb/spotweb/issues/569

--- dbsettings.inc__.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$dbsettings['engine'] = 'pdo_mysql';
-$dbsettings['host'] = 'localhost';
-$dbsettings['dbname'] = 'sinistra_nzbgalaxy';
-$dbsettings['user'] = 'sinistra_nzbgalaxy';
-$dbsettings['pass'] = 'a180be566387';
-$dbsettings['port'] = '3306';
