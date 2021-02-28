5ea0476928cf55edac88f48c3e9e2be5547cb18a
JR <mesa57@hotmail.com>
2021-02-28 17:30:21 +0100

    Fix issue #659 by switching yo Azure translator api V3.0

--- lib/services/Translation/Services_Translation_Microsoft.php
+++ lib/services/Translation/Services_Translation_Microsoft.php
@@ -3,20 +3,16 @@
 class Services_Translation_Microsoft
 {
     // actual translation URL
-    const translateUrl = 'http://api.microsofttranslator.com/v2/Http.svc/TranslateArray?';
+    const translateUrl = 'https://api.cognitive.microsofttranslator.com/translate?api-version=3.0';  
     // oAuth url
-    const authUrl = 'https://api.cognitive.microsoft.com/sts/v1.0/issueToken';
-    // application Scope Url
-    const scopeUrl = 'http://api.microsofttranslator.com';
-    // application grant type
-    const grantType = 'client_credentials';
+    const authUrl = 'https://westeurope.api.cognitive.microsoft.com/sts/v1.0/issueToken';
     // subscription key headers
     const subscriptionKeyHeader = 'Ocp-Apim-Subscription-Key';
 
     /*
      * These id's need to be copied from the Azure market place, please
-     * check the follow blog post
-     *      http://blogs.msdn.com/b/translation/p/gettingstarted1.aspx
+     * check the following
+	 *      https://azure.microsoft.com/nl-nl/services/cognitive-services/translator/#pricing
      */
     private $_subscriptionKey = '';
 
@@ -95,43 +91,14 @@ class Services_Translation_Microsoft
      */
     public function translateSingle($dstLanguage, $text)
     {
-        /*
-         * Try to obtain an translator token
-         */
-        $translaterToken = $this->getAuthToken();
-        if ($translaterToken === false) {
-            return false;
-        } // if
-
-        /*
-         * Actually start translating our message
-         */
-        $params = 'appId='.
-                    '&text='.urlencode($text).
-                    '&to='.urlencode($dstLanguage).
-                    '&contentType='.urlencode('text/plain');
-
-        $svcPrvHttp = new Services_Providers_Http(null);
-        $svcPrvHttp->setBearerAuth($translaterToken);
-        $httpResult = $svcPrvHttp->perform(self::translateUrl.$params, false);
-
-        /*
-         * and use the result
-        */
-        if ($httpResult['successful']) {
-            $xmlReturn = simplexml_load_string($httpResult['data']);
-
-            return (string) $xmlReturn[0];
-        } else {
-            return false;
-        } // else
+        return false; // function not used anymore
     }
-
     // translateSingle
 
+
     /*
-     * translate a single body
-     */
+	 * translate a list of body's
+	 */
     public function translateMultiple($dstLanguage, $list, $field)
     {
         /*
@@ -143,56 +110,33 @@ class Services_Translation_Microsoft
         } // if
 
         /*
-         * Actually start translating our message
+         * Actually start translating the list of messages
          */
-        $doc = new DOMDocument('1.0', 'utf-8');
-        $doc->formatOutput = true;
-
-        $tar = $doc->createElement('TranslateArrayRequest');
-        $tar->appendChild($doc->createElement('AppId', ''));
-        $tar->appendChild($doc->createElement('From', ''));
-
-        $texts = $doc->createElement('Texts');
         foreach ($list as $v) {
-            $str = $doc->createElement('string');
-            $str->appendChild($doc->createTextNode($v[$field]));
-            $str->setAttribute('xmlns', 'http://schemas.microsoft.com/2003/10/Serialization/Arrays');
-
-            $texts->appendChild($str);
-        } // foreach
-        $tar->appendChild($texts);
-        $tar->appendChild($doc->createElement('To', $dstLanguage));
-        $doc->appendChild($tar);
-
+            $obj = new stdClass();
+            $obj->text = $v[$field];
+            $ar[] = $obj;
+        }
         $svcPrvHttp = new Services_Providers_Http(null);
         $svcPrvHttp->setBearerAuth($translaterToken);
         $svcPrvHttp->setMethod('POST');
-        $svcPrvHttp->setRawPostData($doc->saveXML());
-        $svcPrvHttp->setContentType('text/xml');
-        $httpResult = $svcPrvHttp->perform(self::translateUrl, false);
-
-        /*
-         * and use the result
-        */
+        $svcPrvHttp->setRawPostData(json_encode($ar));
+        $svcPrvHttp->setContentType('application/json; charset=UTF-8');
+        $url = self::translateUrl.'&to='.$dstLanguage;
+        $httpResult = $svcPrvHttp->perform($url, false);
         if ($httpResult['successful']) {
-            /*
-             * Loop through all results, and actually translate the
-             * information
-             */
-            $translated = simplexml_load_string($httpResult['data']);
+            $robj = json_decode($httpResult['data'], false);
             $listCounter = 0;
-
-            foreach ($translated->TranslateArrayResponse as $tar) {
-                $list[$listCounter][$field.'_translated'] = (string) $tar->TranslatedText;
-
+            foreach ($robj as $l) {
+                $x[] = $l->translations[0]->text;
+                $list[$listCounter][$field.'_translated'] = (string) $l->translations[0]->text;
                 $listCounter++;
-            } // foreach
-
+            }
             return $list;
         } else {
             return false;
-        } // else
+        }
     }
-
     // translateMultiple
+
 } // class Services_Translation_Microsoft
