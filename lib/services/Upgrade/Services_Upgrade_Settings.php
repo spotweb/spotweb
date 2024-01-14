5e35e28fc43717d692f263a45684b473b7870d26
Sweepr <sweepr@outlook.com>
2024-01-14 14:29:43 +0100

    Update Services_Upgrade_Settings.php
    https://github.com/spotweb/spotweb/issues/704

--- lib/services/Upgrade/Services_Upgrade_Settings.php
+++ lib/services/Upgrade/Services_Upgrade_Settings.php
@@ -38,7 +38,7 @@ class Services_Upgrade_Settings
         $this->remove('action');
         $this->remove('submitedit');
         //Assure usage of the newer black/whitelists when settingsversion is lower then 31.
-        if ($this->_settings->get('settingsversion') < 0.34) {
+        if ($this->_settings->get('settingsversion') < 0.35) {
             $this->remove('blacklist_url');
             $this->remove('whitelist_url');
         } // if
@@ -67,9 +67,9 @@ class Services_Upgrade_Settings
         $this->setIfNot('spot_moderation', 'act');
         $this->setIfNot('prepare_statistics', false);
         $this->setIfNot('external_blacklist', true);
-        $this->setIfNot('blacklist_url', 'http://bit.do/sp4black');
+        $this->setIfNot('blacklist_url', 'http://spotlist.tk/spotnet/blacklist.xml');
         $this->setIfNot('external_whitelist', true);
-        $this->setIfNot('whitelist_url', 'http://bit.do/sp4white');
+        $this->setIfNot('whitelist_url', 'http://spotlist.tk/spotnet/whitelist.xml');
         $this->setIfNot('enable_timing', false);
         $this->setIfNot('cache_path', './cache');
         $this->setIfNot('enable_stacktrace', false);
