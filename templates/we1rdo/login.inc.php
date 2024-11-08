aacaa4320c74c6235c2ebf77c12fc54f180ebb37
CicerBro <177757655+CicerBro@users.noreply.github.com>
2024-11-08 08:55:58 +0000

    Update login.inc.php

--- templates/we1rdo/login.inc.php
+++ templates/we1rdo/login.inc.php
@@ -1,41 +1,41 @@
 <?php
-    require __DIR__.'/includes/form-messages.inc.php';
+require __DIR__.'/includes/form-messages.inc.php';
 
-    /*
-     * Do we need to redirect on success? If so, perform this
-     */
-    if (isset($data['performredirect']) && ($result->isSuccess())) {
-        $tplHelper->redirect($loginform['http_referer']);
+/*
+ * Do we need to redirect on success? If so, perform this
+ */
+if (isset($data['performredirect']) && ($result->isSuccess())) {
+    $tplHelper->redirect($loginform['http_referer']);
 
-        return;
-    } // if
+    return;
+} // if
 
-    $didSubmitForm = showResults($result, $data);
+$didSubmitForm = showResults($result, $data);
 
-    /*
-     * If the form submission was successful, all output
-     * we wanted has already been sent (either the JSON
-     * or the redirect).
-     *
-     * If not, we try to re-render the form again
-     */
-    if (($didSubmitForm) && (!isset($data['renderhtml']))) {
-        return;
-    } // if
+/*
+ * If the form submission was successful, all output
+ * we wanted has already been sent (either the JSON
+ * or the redirect).
+ *
+ * If not, we try to re-render the form again
+ */
+if (($didSubmitForm) && (!isset($data['renderhtml']))) {
+    return;
+} // if
 
-    /*
-     * If no HTML headers are sent just yet, make sure
-     * we send them to the client
-     */
-    if (!isset($data['htmlheaderssent'])) {
-        require_once __DIR__.'/includes/basic-html-header.inc.php';
+/*
+ * If no HTML headers are sent just yet, make sure
+ * we send them to the client
+ */
+if (!isset($data['htmlheaderssent'])) {
+    require_once __DIR__.'/includes/basic-html-header.inc.php';
 
-        $data['renderhtml'] = true;
-    } // if
+    $data['renderhtml'] = true;
+} // if
 ?>
 <h1>Your Spotweb login is different than your NZBFinder login!</h1>
 <br />
-<h2>If you're new, please check your inbox and spam folders first before contacting us.</h2>
+<h2>If you're new, please (re)set your Spotweb password on your NZBFinder profile page.</h2>
 <div class='login'>
     <form class="loginform" name="loginform" action="<?php echo $tplHelper->getPageUrl('login'); ?>" method="post">
         <input type="hidden" name="loginform[xsrfid]" value="<?php echo $tplHelper->generateXsrfCookie('loginform'); ?>">
