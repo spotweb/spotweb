1a5f91dd8206155f113486770dc6e04e8667fbba
CicerBro <177757655+CicerBro@users.noreply.github.com>
2026-02-16 19:04:10 +0000

    Make constructor/render public; reformat class
    Give explicit public visibility to __construct() and render(), remove an unused private property declaration, and adjust brace/indentation style in SpotPage_createuser2.php. Changes are stylistic and aim for clearer, modern PHP code style; no functional logic was altered.

--- lib/page/SpotPage_createuser2.php
+++ lib/page/SpotPage_createuser2.php
@@ -1,53 +1,54 @@
 <?php
 
-class SpotPage_createuser2 extends SpotPage_Abs {
-    private $_createUserForm;
+class SpotPage_createuser2 extends SpotPage_Abs
+{
 
-    function __construct(Dao_Factory $daoFactory, Services_Settings_Container $settings, array $currentSession) {
+    public function __construct(Dao_Factory $daoFactory, Services_Settings_Container $settings, array $currentSession)
+    {
         parent::__construct($daoFactory, $settings, $currentSession);
-        //              $this->_createUserForm = $params['createuserform'];
-if (!isset($_POST['secure']) || $_POST['secure'] != 1) {
-    http_response_code(401);
-    echo "Not authorized!";
-    die;
-}
+        if (!isset($_POST['secure']) || $_POST['secure'] != 1) {
+            http_response_code(401);
+            echo "Not authorized!";
+            die;
+        }
     } # ctor
 
-    function render() {
-	if (isset($_POST['secure']) && $_POST['secure'] == 1) {
+    public function render()
+    {
+        if (isset($_POST['secure']) && $_POST['secure'] == 1) {
 
-	$result = new Dto_FormResult('notsubmitted');
+            $result = new Dto_FormResult('notsubmitted');
 
-        /*
-         * Create a default SpotUser so the form is always able to render
-         * the values of the form
-         */
-        $spotUser = array(
-            'username' => $_POST['username'],
-            'firstname' => $_POST['username'],
-            'lastname' => 'None',
-            'mail' => $_POST['mail']
-        );
+            /*
+             * Create a default SpotUser so the form is always able to render
+             * the values of the form
+             */
+            $spotUser = array(
+                'username' => $_POST['username'],
+                'firstname' => $_POST['username'],
+                'lastname' => 'None',
+                'mail' => $_POST['mail']
+            );
 
 
-        $other = array(
-            "xsrfid" => "none",
-            "submitcreate" => "Add",
-            "action" => "create",
-            "http_referer" => "https://spotweb.nzbfinder.ws/?page=createuser2"
-        );
+            $other = array(
+                "xsrfid" => "none",
+                "submitcreate" => "Add",
+                "action" => "create",
+                "http_referer" => "https://spotweb.nzbfinder.ws/?page=createuser2"
+            );
 
-        # Instantiate the Spot usersystem
-        $svcActnCreateUser = new Services_Actions_CreateUser($this->_settings, $this->_daoFactory);
+            # Instantiate the Spot usersystem
+            $svcActnCreateUser = new Services_Actions_CreateUser($this->_settings, $this->_daoFactory);
 
-        $spotUser = array_merge($spotUser, $other);
-        $result = $svcActnCreateUser->createNewUser($spotUser, $this->_currentSession);
+            $spotUser = array_merge($spotUser, $other);
+            $result = $svcActnCreateUser->createNewUser($spotUser, $this->_currentSession);
 
-        #- display stuff -#
-        $this->template('createuser2', array(
-            'createuserform' => $spotUser,
-            'result' => $result
-        ));
-    } # render
-}
+            #- display stuff -#
+            $this->template('createuser2', array(
+                'createuserform' => $spotUser,
+                'result' => $result
+            ));
+        } # render
+    }
 } # class SpotPage_createuser2
