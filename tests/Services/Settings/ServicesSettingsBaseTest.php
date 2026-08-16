3d8e3b5122fd1dbc42490c956006a82645f2b306
J.H.S. Schlepers <bschlepe@server01.schlepers.de>
2026-08-16 16:02:46 +0200

    Standardize MySQL schema on InnoDB utf8mb4

---
+++ tests/Services/Settings/ServicesSettingsBaseTest.php
@@ -0,0 +1,79 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ServicesSettingsBaseTestContainer implements Services_Settings_IContainer
+{
+    private $settings;
+
+    public function __construct(array $settings)
+    {
+        $this->settings = $settings;
+    }
+
+    public function initialize(array $cfg)
+    {
+    }
+
+    public function getAllSettings()
+    {
+        return $this->settings;
+    }
+
+    public function remove($name)
+    {
+        unset($this->settings[$name]);
+    }
+
+    public function set($name, $value)
+    {
+        $this->settings[$name] = $value;
+    }
+}
+
+class ServicesSettingsBaseTestBlackWhiteListDao implements Dao_BlackWhiteList
+{
+    public function removeOldList($listUrl, $idtype)
+    {
+    }
+
+    public function updateExternalList($newlist, $idtype)
+    {
+    }
+
+    public function addSpotterToList($spotterId, $ourUserId, $origin, $idType)
+    {
+    }
+
+    public function removeSpotterFromList($spotterId, $ourUserId)
+    {
+    }
+
+    public function getSpotterList($ourUserId)
+    {
+    }
+
+    public function getBlacklistForSpotterId($userId, $spotterId)
+    {
+    }
+}
+
+class ServicesSettingsBaseTest extends TestCase
+{
+    public function testSchemaVersionBumpRequiresUpgradeFromPreviousSchema()
+    {
+        $settings = new Services_Settings_Container();
+        $settings->addSource(new ServicesSettingsBaseTestContainer([
+            'schemaversion' => '0.69',
+        ]));
+
+        $service = new Services_Settings_Base($settings, new ServicesSettingsBaseTestBlackWhiteListDao());
+
+        $this->assertSame('0.70', SPOTDB_SCHEMA_VERSION);
+        $this->assertFalse($service->schemaValid());
+
+        $settings->set('schemaversion', SPOTDB_SCHEMA_VERSION);
+
+        $this->assertTrue($service->schemaValid());
+    }
+}
